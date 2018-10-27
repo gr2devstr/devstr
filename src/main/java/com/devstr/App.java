da6aa1970df3f20cead697bb500ab207d1c38ca2
Nik Mko <holinkonik@gmail.com>
2018-10-28 00:19:04 +0300

    28.10.2018 Initial commit

---
+++ src/main/java/com/devstr/App.java
@@ -0,0 +1,9 @@
+package com.devstr;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/com/devstr/AppTest.java
@@ -0,0 +1,17 @@
+package com.devstr;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void emptyTest() {
+
+    }
+
+}
