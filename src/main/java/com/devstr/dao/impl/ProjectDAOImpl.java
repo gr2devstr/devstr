4f000ac62b7d464cb6a67ec5278fdb382a0b4b45
Robert <rtalabishka@gmail.com>
2018-11-23 17:59:32 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-53 1)Add github dependency to pom.xml 2)Create GitHubService interface 3)Implement GitHubService in progress...

--- src/main/java/com/devstr/dao/impl/ProjectDAOImpl.java
+++ src/main/java/com/devstr/dao/impl/ProjectDAOImpl.java
@@ -2,6 +2,7 @@ package com.devstr.dao.impl;
 
 import com.devstr.dao.ProjectDAO;
 import com.devstr.model.Project;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Set;
@@ -11,11 +12,41 @@ import java.util.Set;
  */
 public class ProjectDAOImpl implements ProjectDAO {
 
+    @Autowired
     JdbcTemplate jdbcTemplate;
 
     @Override
     public void createProject(String name, int managerId, int techId, Set<Integer> devs, String repoName, String gitLogin, String gitPass, String jiraLogin, String jiraPass) {
-
+        String sql = "INSERT ALL " +
+                "INTO OBJECTS(NAME, OBJECT_TYPE_ID) " +
+                " VALUES(?,2)" +
+                "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, DATE_VALUE)" +
+                "VALUES(5, PROJ_ID, SYSDATE)" +
+                "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, LIST_VALUE_ID)" +
+                "VALUES(7,PROJ_ID,5)" +
+                "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, DATE_VALUE)" +
+                "VALUES(8,PROJ_ID,?)" +
+                "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE)" +
+                "VALUES(9,PROJ_ID,?)" +
+                "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE)" +
+                "VALUES(10,PROJ_ID,?)" +
+                "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE)" +
+                "VALUES(11,PROJ_ID,?)" +
+                "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE)" +
+                "VALUES(12,PROJ_ID,?)" +
+                "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE)" +
+                "VALUES(13,PROJ_ID,?)" +
+                "SELECT proj.OBJECT_ID AS PROJ_ID" +
+                "        FROM OBJECTS proj WHERE proj.NAME = ?;";
+        jdbcTemplate.update(sql, new Object[]{
+                name,
+                null,
+                gitLogin,
+                jiraPass,
+                jiraLogin,
+                jiraPass,
+                name
+        });
     }
 
     @Override
---
+++ src/main/java/com/devstr/services/impl/GitServiceImpl.java
@@ -0,0 +1,70 @@
+package com.devstr.services.impl;
+
+import com.devstr.model.Commit;
+import com.devstr.model.CommitClass;
+import com.devstr.services.interfaces.iGitService;
+import org.kohsuke.github.GHCommit;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Robert in 23.11.2018
+ */
+public class GitServiceImpl implements iGitService {
+
+    private String repositoryName;
+    private String token;
+
+
+    public GitServiceImpl(String repositoryName, String token) {
+        this.repositoryName = repositoryName;
+        this.token = token;
+    }
+
+    @Override
+    public GHRepository getGHRepository() throws IOException {
+        return GitHub.connectUsingOAuth(token).getRepository(repositoryName);
+    }
+
+    @Override
+    public GHCommit getLastGHCommit() throws IOException {
+        return getGHRepository().listCommits().asList().get(0);
+    }
+
+    @Override
+    public GHCommit getGHCommit(String commitSHA) throws IOException {
+        return getGHRepository().getCommit(commitSHA);
+    }
+
+    @Override
+    public List<CommitClass> getAllCommitClasses(GHCommit commit) throws IOException {
+
+        List<CommitClass> commitClasses = new ArrayList<>();
+        List<GHCommit.File> files = getLastGHCommit().getFiles();
+
+        for (GHCommit.File file : files) {
+            commitClasses.add(CommitClass.builder()
+                    .setClassName(file.getFileName())
+                    .setAddedLines(file.getLinesAdded())
+                    .setChangedLines(file.getLinesChanged())
+                    .setDeletedLines(file.getLinesDeleted())
+                    .build());
+        }
+        return commitClasses;
+    }
+
+    @Override
+    public GHUser getCommiter(GHCommit commit) {
+        return null;
+    }
+
+    @Override
+    public Commit getCommit() {
+        return null;
+    }
+}
