b6a4bd735a90b5762c52042df2290917024e50af
Aleksey Rudyk <10157945+AlekseyRudyk@users.noreply.github.com>
2018-11-26 23:14:31 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-29
    Created interface for issue POJO and its implementation.

--- src/main/java/com/devstr/model/Issue.java
+++ src/main/java/com/devstr/model/Issue.java
@@ -3,180 +3,36 @@ package com.devstr.model;
 import com.devstr.model.enumerations.IssuePriority;
 import com.devstr.model.enumerations.IssueStatus;
 import com.devstr.model.enumerations.IssueType;
+
 import java.math.BigInteger;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.Set;
 
-public class Issue {
+public interface Issue {
 
-    private BigInteger issueId;
-    private String issueKey;
-    private IssueType type;
-    private IssueStatus status;
-    private IssuePriority priority;
-    private Date startDate;
-    private Date dueDate;
-    private BigInteger projectId;
-    private BigInteger userId;
-    private String reporter;
-    private Set<Commit> commits;
+    BigInteger getIssueId();
 
-    private Issue(){
-    }
+    String getIssueKey();
 
-    public BigInteger getIssueId() {
-        return issueId;
-    }
+    IssueType getType();
 
-    public String getIssueKey() {
-        return issueKey;
-    }
+    IssueStatus getStatus();
 
-    public IssueType getType() {
-        return type;
-    }
+    IssuePriority getPriority();
 
-    public IssueStatus getStatus() {
-        return status;
-    }
+    Date getStartDate();
 
-    public IssuePriority getPriority() {
-        return priority;
-    }
+    Date getDueDate();
 
-    public Date getStartDate() {
-        return startDate;
-    }
+    BigInteger getProjectId();
 
-    public Date getDueDate() {
-        return dueDate;
-    }
+    BigInteger getUserId();
 
-    public BigInteger getProjectId() {
-        return projectId;
-    }
+    String getReporter();
 
-    public BigInteger getUserId() {
-        return userId;
-    }
+    Set<Commit> getCommits();
 
-    public String getReporter() {
-        return reporter;
-    }
+    void setCommit(Commit commit);
 
-    public Set<Commit> getCommits() {
-        return commits;
-    }
-
-    public void setCommit(Commit commit) {
-        if (this.commits == null) {
-            this.commits = new HashSet<>();
-        }
-        this.commits.add(commit);
-    }
-
-    public void setCommits(Set<Commit> commits) {
-        if (this.commits == null) {
-            this.commits = new HashSet<>();
-        }
-        this.commits.addAll(commits);
-    }
-
-    public static IssueBuilder builder() {
-        return new Issue().new IssueBuilder();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Issue issue = (Issue) o;
-
-        return getIssueId().equals(issue.getIssueId());
-    }
-
-    @Override
-    public int hashCode() {
-        return getIssueId().hashCode();
-    }
-
-    public class IssueBuilder {
-
-        private IssueBuilder() {
-        }
-
-        public IssueBuilder setIssueId(BigInteger id) {
-            Issue.this.issueId = id;
-            return this;
-        }
-
-        public IssueBuilder setIssueKey(String key) {
-            Issue.this.issueKey = key;
-            return this;
-        }
-
-        public IssueBuilder setIssueType(IssueType type) {
-            Issue.this.type = type;
-            return this;
-        }
-
-        public IssueBuilder setIssueStatus(IssueStatus status) {
-            Issue.this.status = status;
-            return this;
-        }
-
-        public IssueBuilder setIssuePriority(IssuePriority priority) {
-            Issue.this.priority = priority;
-            return this;
-        }
-
-        public IssueBuilder setStartDate(Date startDate) {
-            Issue.this.startDate = startDate;
-            return this;
-        }
-
-        public IssueBuilder setDueDate(Date dueDate) {
-            Issue.this.dueDate = dueDate;
-            return this;
-        }
-
-        public IssueBuilder setProjectId(BigInteger id) {
-            Issue.this.projectId = id;
-            return this;
-        }
-
-        public IssueBuilder setUserId(BigInteger id) {
-            Issue.this.userId = id;
-            return this;
-        }
-
-        public IssueBuilder setReporter(String reporter) {
-            Issue.this.reporter = reporter;
-            return this;
-        }
-
-        public IssueBuilder setCommit(Commit commit) {
-            if (Issue.this.commits == null) {
-                Issue.this.commits = new HashSet<>();
-            }
-            Issue.this.commits.add(commit);
-            return this;
-        }
-
-        public IssueBuilder setCommits(Set<Commit> commits) {
-            if (Issue.this.commits == null) {
-                Issue.this.commits = new HashSet<>();
-            }
-            Issue.this.commits.addAll(commits);
-            return this;
-        }
-
-        public Issue build() {
-            return Issue.this;
-        }
-
-    }
-
+    void setCommits(Set<Commit> commits);
 }
---
+++ src/main/java/com/devstr/model/impl/IssueImpl.java
@@ -0,0 +1,213 @@
+package com.devstr.model.impl;
+
+import com.devstr.model.Commit;
+import com.devstr.model.Issue;
+import com.devstr.model.enumerations.IssuePriority;
+import com.devstr.model.enumerations.IssueStatus;
+import com.devstr.model.enumerations.IssueType;
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class IssueImpl implements Issue {
+
+    private BigInteger issueId;
+    private String issueKey;
+    private IssueType type;
+    private IssueStatus status;
+    private IssuePriority priority;
+    private Date startDate;
+    private Date dueDate;
+    private BigInteger projectId;
+    private BigInteger userId;
+    private String reporter;
+    private Set<Commit> commits;
+
+    @Override
+    public BigInteger getIssueId() {
+        return issueId;
+    }
+
+    @Override
+    public String getIssueKey() {
+        return issueKey;
+    }
+
+    @Override
+    public IssueType getType() {
+        return type;
+    }
+
+    @Override
+    public IssueStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public IssuePriority getPriority() {
+        return priority;
+    }
+
+    @Override
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    @Override
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    @Override
+    public BigInteger getProjectId() {
+        return projectId;
+    }
+
+    @Override
+    public BigInteger getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String getReporter() {
+        return reporter;
+    }
+
+    @Override
+    public Set<Commit> getCommits() {
+        return commits;
+    }
+
+    @Override
+    public void setCommit(Commit commit) {
+        if (this.commits == null) {
+            this.commits = new HashSet<>();
+        }
+        this.commits.add(commit);
+    }
+
+    @Override
+    public void setCommits(Set<Commit> commits) {
+        if (this.commits == null) {
+            this.commits = new HashSet<>();
+        }
+        this.commits.addAll(commits);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        IssueImpl issue = (IssueImpl) o;
+
+        return getIssueId().equals(issue.getIssueId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getIssueId().hashCode();
+    }
+
+    public static class Builder {
+
+        private BigInteger issueId;
+        private String issueKey;
+        private IssueType type;
+        private IssueStatus status;
+        private IssuePriority priority;
+        private Date startDate;
+        private Date dueDate;
+        private BigInteger projectId;
+        private BigInteger userId;
+        private String reporter;
+        private Set<Commit> commits;
+
+        private Builder() {}
+
+        public Builder setIssueId(BigInteger id) {
+            this.issueId = id;
+            return this;
+        }
+
+        public Builder setIssueKey(String key) {
+            this.issueKey = key;
+            return this;
+        }
+
+        public Builder setIssueType(IssueType type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder setIssueStatus(IssueStatus status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder setIssuePriority(IssuePriority priority) {
+            this.priority = priority;
+            return this;
+        }
+
+        public Builder setStartDate(Date startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+
+        public Builder setDueDate(Date dueDate) {
+            this.dueDate = dueDate;
+            return this;
+        }
+
+        public Builder setProjectId(BigInteger id) {
+            this.projectId = id;
+            return this;
+        }
+
+        public Builder setUserId(BigInteger id) {
+            this.userId = id;
+            return this;
+        }
+
+        public Builder setReporter(String reporter) {
+            this.reporter = reporter;
+            return this;
+        }
+
+        public Builder setCommit(Commit commit) {
+            if (this.commits == null) {
+                this.commits = new HashSet<>();
+            }
+            this.commits.add(commit);
+            return this;
+        }
+
+        public Builder setCommits(Set<Commit> commits) {
+            if (this.commits == null) {
+                this.commits = new HashSet<>();
+            }
+            this.commits.addAll(commits);
+            return this;
+        }
+
+        public Issue build() {
+            return new IssueImpl(this);
+        }
+
+    }
+
+    private IssueImpl(Builder builder){
+        this.issueId = builder.issueId;
+        this.issueKey = builder.issueKey;
+        this.type = builder.type;
+        this.status = builder.status;
+        this.priority = builder.priority;
+        this.startDate = builder.startDate;
+        this.dueDate = builder.dueDate;
+        this.projectId = builder.projectId;
+        this.userId = builder.userId;
+        this.reporter = builder.reporter;
+        this.commits = builder.commits;
+    }
+}
