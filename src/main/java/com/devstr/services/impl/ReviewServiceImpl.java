a7f85d3aa5fa577d722a3094f180016a6077735d
Vlad <vladismat@gmail.com>
2018-11-27 18:22:41 +0200

    https://nc-std.atlassian.net/projects/DEVSTR/issues/DEVSTR-49 Added ReviewService interface and its impl. Implemented methods check information of reviews, and use methods of DAO

---
+++ src/main/java/com/devstr/services/impl/ReviewServiceImpl.java
@@ -0,0 +1,84 @@
+package com.devstr.services.impl;
+
+import com.devstr.DevstrLogger;
+import com.devstr.dao.ReviewDAO;
+import com.devstr.dao.impl.ReviewDAOImpl;
+import com.devstr.model.ProjectReview;
+import com.devstr.model.Review;
+import com.devstr.model.UserReview;
+import com.devstr.model.enumerations.ObjectType;
+import com.devstr.services.interfaces.ReviewService;
+
+import javax.sql.DataSource;
+import java.math.BigInteger;
+import java.util.List;
+
+public class ReviewServiceImpl implements ReviewService {
+    private static DevstrLogger LOGGER = new DevstrLogger(ReviewServiceImpl.class.getName());
+    ReviewDAO reviewDAO;
+
+    public ReviewServiceImpl(DataSource dataSource) {
+        reviewDAO = new ReviewDAOImpl(dataSource);
+    }
+
+    @Override
+    public void createReview(BigInteger authorId, BigInteger receiverId, BigInteger projectId, String comment, int[] marks, ObjectType objType) {
+        if (authorId == null) {
+            LOGGER.warn("authorId is null");
+            return;
+        } if (receiverId == null) {
+            LOGGER.warn("authorId is null");
+            return;
+        } if (objType.equals(ObjectType.USER_REVIEW)) {
+            if (projectId == null){
+                LOGGER.warn("projectId is null. There are no user reviews without project");
+                return;
+            }
+            reviewDAO.createUserReview(authorId, receiverId, projectId, comment, marks);
+        } if (objType.equals(ObjectType.PROJECT_REVIEW)) {
+            reviewDAO.createProjectReview(authorId, receiverId, comment, marks);
+        }
+        LOGGER.warn("Wrong object type");
+    }
+
+    @Override
+    public Review readReviewById(BigInteger id) {
+        if (id != null)
+            return reviewDAO.readReviewById(id);
+        LOGGER.warn("Id is null");
+        return null;
+    }
+
+    @Override
+    public List<Review> readReviewsByRecId(BigInteger id) {
+        if (id != null)
+            return reviewDAO.readReviewsByRecId(id);
+        LOGGER.warn("Id is null");
+        return null;
+    }
+
+    @Override
+    public List<Review> readReviewsByAuthorId(BigInteger id) {
+        if (id != null)
+            return reviewDAO.readReviewsByAuthorId(id);
+        LOGGER.warn("Id is null");
+        return null;
+    }
+
+    @Override
+    public List<ProjectReview> readReviewsByProjectId(BigInteger id) {
+        if (id != null)
+            return reviewDAO.readReviewsByProjectId(id);
+        LOGGER.warn("Id is null");
+        return null;
+    }
+
+    @Override
+    public List<UserReview> readUserReviewsByProjectId(BigInteger id) {
+        if (id != null) {
+            return reviewDAO.readUserReviewsByProjectId(id);
+        }
+        LOGGER.warn("Id is null");
+        return null;
+    }
+}
---
+++ src/main/java/com/devstr/services/interfaces/ReviewService.java
@@ -0,0 +1,65 @@
+package com.devstr.services.interfaces;
+
+import com.devstr.model.ProjectReview;
+import com.devstr.model.Review;
+import com.devstr.model.UserReview;
+import com.devstr.model.enumerations.ObjectType;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public interface ReviewService {
+
+    /**
+     * Method that creates new review
+     *
+     * @param authorId id of the review author
+     * @param receiverId id of receiverId
+     * @param projectId id of project where review where made(equal with receiverId if it is project review)
+     * @param comment text of the review
+     * @param marks 3 or 4 element size array with marks to the project
+     * @param objType object type(USER_REVIEW or PROJECT_REVIEW)
+     */
+    void createReview(BigInteger authorId, BigInteger receiverId, BigInteger projectId, String comment, int[] marks,
+                      ObjectType objType);
+
+    /**
+     * Method gets the review by review's id
+     *
+     * @param id id of review
+     * @return review with such id in database, null if there is no review with such id
+     */
+    Review readReviewById(BigInteger id);
+
+    /**
+     * Gets all reviews of user with specified id
+     *
+     * @param id id of user that received the reviews
+     * @return list with the reviews
+     */
+    List<Review> readReviewsByRecId(BigInteger id);
+
+    /**
+     * Gets all the reviews written by certain author
+     *
+     * @param id author id in table OBJECTS
+     * @return list with the reviews
+     */
+    List<Review> readReviewsByAuthorId(BigInteger id);
+
+    /**
+     * Gets all the reviews of a project(Project reviews only!)
+     *
+     * @param id id of the project
+     * @return
+     */
+    List<ProjectReview> readReviewsByProjectId(BigInteger id);
+
+    /**
+     * Gets all user reviews left during certain project
+     *
+     * @param id id of the project
+     * @return list with all user reviews written during project
+     */
+    List<UserReview> readUserReviewsByProjectId(BigInteger id);
+}
