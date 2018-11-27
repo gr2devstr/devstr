6712eb8eb995e59cd8d8985dc30d003102c7af17
ColdMentha <31902812+ColdMentha@users.noreply.github.com>
2018-11-27 02:31:10 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-47 Create Token POJO

---
+++ src/main/java/com/devstr/model/impl/TokenImpl.java
@@ -0,0 +1,84 @@
+package com.devstr.model.impl;
+
+import com.devstr.model.Token;
+
+import java.math.BigInteger;
+import java.util.Objects;
+
+public class TokenImpl implements Token {
+    private BigInteger tokenId;
+    private BigInteger projectId;
+    private String serviceName;
+    private String tokenEncode;
+    private String tokensName;
+
+
+    @Override
+    public BigInteger getTokenId() {
+        return tokenId;
+    }
+
+    @Override
+    public BigInteger getProjectId() {
+        return projectId;
+    }
+
+    @Override
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    @Override
+    public String getTokenEncode() {
+        return tokenEncode;
+    }
+
+    @Override
+    public String getTokensName() {
+        return tokensName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TokenImpl token = (TokenImpl) o;
+
+        return getProjectId().equals(token.tokenId);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return getTokenId().hashCode();
+    }
+
+    public static class Builder{
+        private BigInteger tokenId;
+        private BigInteger projectId;
+        private String serviceName;
+        private String tokenEncode;
+        private String tokensName;
+
+        public Builder(BigInteger projectId, String serviceName, String tokenEncode, String tokensName) {
+            this.projectId = projectId;
+            this.serviceName = serviceName;
+            this.tokenEncode = tokenEncode;
+            this.tokensName = tokensName;
+        }
+
+        public Builder setTokenId(BigInteger tokenId) {
+            this.tokenId = tokenId;
+            return this;
+        }
+    }
+
+    private TokenImpl(Builder builder) {
+        this.tokenId = builder.tokenId;
+        this.projectId = builder.projectId;
+        this.serviceName = builder.serviceName;
+        this.tokenEncode = builder.tokenEncode;
+        this.tokensName = builder.tokensName;
+    }
+}
