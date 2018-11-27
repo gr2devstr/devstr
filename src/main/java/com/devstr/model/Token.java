1680781d58e22827420ed1840b0874ca323ecee6
ColdMentha <31902812+ColdMentha@users.noreply.github.com>
2018-11-27 02:30:30 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-47 Create Token Interface

---
+++ src/main/java/com/devstr/model/Token.java
@@ -0,0 +1,15 @@
+package com.devstr.model;
+
+import java.math.BigInteger;
+
+public interface Token {
+    BigInteger getTokenId();
+
+    BigInteger getProjectId();
+
+    String getServiceName();
+
+    String getTokenEncode();
+
+    String getTokensName();
+}
