901945768c66eac6c8c84ac595ba56d1a53e08ae
Aleksey Rudyk <10157945+AlekseyRudyk@users.noreply.github.com>
2018-11-30 16:57:36 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-54
    Implement methods at IssueDao

--- src/main/java/com/devstr/dao/impl/IssueDAOImpl.java
+++ src/main/java/com/devstr/dao/impl/IssueDAOImpl.java
@@ -2,12 +2,19 @@ package com.devstr.dao.impl;
 
 import com.devstr.dao.IssueDAO;
 import com.devstr.model.Issue;
+import com.devstr.model.enumerations.IssuePriority;
+import com.devstr.model.enumerations.IssueStatus;
+import com.devstr.model.enumerations.IssueType;
+import com.devstr.model.impl.IssueImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Transactional
@@ -29,21 +36,43 @@ public class IssueDAOImpl implements IssueDAO {
 
     @Override
     public List<Issue> readIssuesByProject(BigInteger projectId) {
-        return null;
+        return jdbcTemplate.queryForList(READ_ISSUES_BY_PROJECT,new Object[]{projectId},Issue.class);
     }
 
     @Override
     public List<Issue> readIssuesByUser(BigInteger userId) {
-        return null;
+        return jdbcTemplate.queryForList(READ_ISSUES_BY_USER,new Object[]{userId},Issue.class);
     }
 
     @Override
     public Issue readIssueById(BigInteger id) {
-        return null;
+        RowMapper<Issue> mapper = new IssueMapper();
+        return jdbcTemplate.query(READ_ISSUE_BY_ID,new Object[]{id},mapper)
+                .get(0);
     }
 
     @Override
     public void updateIssue(Issue issue) {
 
     }
+
+    class IssueMapper implements RowMapper<Issue>{
+
+        @Override
+        public Issue mapRow(ResultSet resultSet, int i) throws SQLException {
+            return new IssueImpl.Builder()
+                    .setIssueId(BigInteger.valueOf(resultSet.getLong(1)))
+                    .setIssueKey(resultSet.getString(2))
+                    .setProjectId(BigInteger.valueOf(resultSet.getLong(3)))
+                    .setIssueType(IssueType.values()[resultSet.getInt(4)])
+                    .setIssueStatus(IssueStatus.values()[resultSet.getInt(5)])
+                    .setIssuePriority(IssuePriority.values()[resultSet.getInt(6)])
+                    .setStartDate(resultSet.getDate(7))
+                    .setDueDate(resultSet.getDate(8))
+                    .setUserId(BigInteger.valueOf(resultSet.getLong(9)))
+                    .setReporterId(BigInteger.valueOf(resultSet.getLong(10)))
+                    .build();
+        }
+    }
+
 }
