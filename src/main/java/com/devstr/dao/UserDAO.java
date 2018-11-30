e88758a66bd987801d08d507ba08a4157875599d
Nikolay <holinkonik@gmail.com>
2018-11-30 16:09:19 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-30 Added SQL queries to the interface UserDAO. Added UserDAOImpl with inner RowMapper.

--- src/main/java/com/devstr/dao/UserDAO.java
+++ src/main/java/com/devstr/dao/UserDAO.java
@@ -2,12 +2,12 @@ package com.devstr.dao;
 
 import com.devstr.model.User;
 import com.devstr.model.enumerations.UserRole;
-
 import java.math.BigInteger;
 
 public interface UserDAO {
+
     /**
-     * Method writes User to database
+     * Creates User in the DB
      *
      * @param login user's login(stores in db as object.name)
      * @param firstName user's first name
@@ -17,39 +17,298 @@ public interface UserDAO {
      * @param userRole user's role
      * @throws IllegalArgumentException if one of the params is null
      */
-    void createUser(String login, String firstName, String lastName, String email, String password, UserRole userRole);
+    void createUser(String login, String firstName, String lastName, String email, UserRole userRole, String password);
 
     /**
-     * Get user by id from the DB
+     * Gets user by id from the DB
      *
-     * @param id object_id of user in table "Objects" of the DB
+     * @param id user's id in the system(objects.object_id attribute in the DB)
      * @return User with certain id
-     * @throws IllegalArgumentException if id <=0 or bigger than size of the objects table
+     *
      */
     User readUserById(BigInteger id);
 
     /**
-     * Get user with a certain login from the DB
+     * Gets user by login from the DB
      *
-     * @param login login of a user in the system(objects.name attribute in the DB)
+     * @param login user's login in the system(objects.name attribute in the DB)
      * @return User with certain login
-     * @throws IllegalArgumentException if login is null
+     *
      */
     User readUserByLogin(String login);
 
     /**
-     * Update user in the database, when id got no changes
+     * Updates user password in the DB by user id
      *
-     * @param user updated User instance with the old id
-     * @throws IllegalArgumentException if user is null
+     * @param id user's id in the system(objects.object_id attribute in the DB)
+     * @param password new password for user
+     *
      */
-    void updateUser(User user);
+    void updateUserPassword(BigInteger id, String password);
+
+    /**
+     * Updates user first name in the DB by user id
+     *
+     * @param id user's id in the system(objects.object_id attribute in the DB)
+     * @param firstName new first name for user
+     */
+    void updateUserFirstName(BigInteger id, String firstName);
+
+    /**
+     * Updates user last name in the DB by user id
+     *
+     * @param id user's id in the system(objects.object_id attribute in the DB)
+     * @param lastName new last name for user
+     */
+    void updateUserLastName(BigInteger id, String lastName);
+
+    /**
+     * Updates user role in the DB by user id
+     *
+     * @param id user's id in the system(objects.object_id attribute in the DB)
+     * @param role new role for user
+     *
+     */
+    void updateUserRole(BigInteger id, UserRole role);
 
     /**
      * Sets user into inactive mode
      *
-     * @param user User instance that must be deactivated
-     * @throws IllegalArgumentException if user is null
+     * @param id user's id in the system(objects.object_id attribute in the DB)
+     *
      */
-    void inactivateUser(User user);
+    void inactivateUser(BigInteger id);
+
+    String CREATE_USER = "BEGIN " +
+            "INSERT INTO OBJECTS(OBJECT_TYPE_ID, NAME) " +
+            // 1 = USER
+            "VALUES(1, ?); " +
+            "INSERT ALL " +
+            "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE) " +
+            // 1 =  FIRST_NAME
+            "VALUES(1, OBJ_ID.CURRVAL , ?) " +
+            "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE) " +
+            // 2 = LAST_NAME
+            "VALUES(2, OBJ_ID.CURRVAL , ?) " +
+            "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE) " +
+            // 3 = EMAIL
+            "VALUES(3, OBJ_ID.CURRVAL , ?) " +
+            "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, LIST_VALUE_ID) " +
+            // 4 = ROLE
+            "VALUES(4, OBJ_ID.CURRVAL , ?) " +
+            "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, DATE_VALUE) " +
+            // 5 = CREATION_DATE
+            "VALUES(5, OBJ_ID.CURRVAL , SYSDATE) " +
+            "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE) " +
+            // 6 = PASSWORD
+            "VALUES(6, OBJ_ID.CURRVAL , ?) " +
+            "INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, LIST_VALUE_ID) " +
+            // 7 = STATUS; 5 = ACTIVE
+            "VALUES(7, OBJ_ID.CURRVAL , 5) " +
+            "SELECT * FROM DUAL; " +
+            "END;";
+
+    String READ_USER_BY_ID = "SELECT " +
+            "U.OBJECT_ID AS USER_ID, " +
+            "U.NAME AS LOGIN, " +
+            "PASS.VALUE AS PASSWORD, " +
+            "EMAIL.VALUE AS EMAIL, " +
+            "FIRST_NAME.VALUE AS FIRST_NAME, " +
+            "LAST_NAME.VALUE AS LAST_NAME, " +
+            "USER_ROLE.VALUE AS USER_ROLE, " +
+            "HIRE_DATE.DATE_VALUE AS HIRE_DATE, " +
+            "STATUS.VALUE AS STATUS, " +
+            "CASE WHEN EXISTS (" +
+            "SELECT U.OBJECT_ID" +
+            "FROM OBJECTS U, " +
+            "OBJREFERENCE PROJ_ID, " +
+            "OBJECTS PROJ, " +
+            "ATTRIBUTES PROJ_STATUS " +
+            // 1 = USER
+            "WHERE U.OBJECT_TYPE_ID = 1 " +
+            "AND U.OBJECT_ID = ? " +
+            // 26 = PROJECT_USERS
+            "AND PROJ_ID.ATTRN_ID = 26 " +
+            "AND PROJ_ID.REFERENCE = U.OBJECT_ID " +
+            // 2 = PROJECT
+            "AND PROJ.OBJECT_TYPE_ID = 2 " +
+            "AND PROJ.OBJECT_ID = PROJ_ID.OBJECT_ID " +
+            // 7 = STATUS
+            "AND PROJ_STATUS.ATTRN_ID = 7 " +
+            "AND PROJ_STATUS.OBJECT_ID = PROJ.OBJECT_ID " +
+            // 5 = ACTIVE
+            "AND PROJ_STATUS.LIST_VALUE_ID = 5" +
+            ") THEN (" +
+            "SELECT PROJ.OBJECT_ID " +
+            "FROM OBJECTS U, " +
+            "OBJREFERENCE PROJ_ID, " +
+            "OBJECTS PROJ, " +
+            "ATTRIBUTES PROJ_STATUS " +
+            // 1 = USER
+            "WHERE U.OBJECT_TYPE_ID = 1 " +
+            "AND U.OBJECT_ID = ? " +
+            // 26 = PROJECT_USERS
+            "AND PROJ_ID.ATTRN_ID = 26 " +
+            "AND PROJ_ID.REFERENCE = U.OBJECT_ID " +
+            // 2 = PROJECT
+            "AND PROJ.OBJECT_TYPE_ID = 2 " +
+            "AND PROJ.OBJECT_ID = PROJ_ID.OBJECT_ID " +
+            // 7 = STATUS
+            "AND PROJ_STATUS.ATTRN_ID = 7 " +
+            "AND PROJ_STATUS.OBJECT_ID = PROJ.OBJECT_ID " +
+            // 5 = ACTIVE
+            "AND PROJ_STATUS.LIST_VALUE_ID = 5 " +
+            ") ELSE NULL " +
+            "END AS PROJ_ID " +
+            "FROM OBJECTS U, " +
+            "ATTRIBUTES PASS, " +
+            "ATTRIBUTES EMAIL, " +
+            "ATTRIBUTES FIRST_NAME, " +
+            "ATTRIBUTES LAST_NAME, " +
+            "LISTS USER_ROLE, ATTRIBUTES USER_ROLE_ID, " +
+            "LISTS STATUS, ATTRIBUTES STATUS_ID " +
+            // 1 = USER
+            "WHERE U.OBJECT_TYPE_ID = 1 " +
+            "AND U.OBJECT_ID = ? " +
+            // 6 = PASSWORD
+            "AND PASS.ATTRN_ID = 6 " +
+            "AND PASS.OBJECT_ID = U.OBJECT_ID " +
+            // 3 = EMAIL
+            "AND EMAIL.ATTRN_ID = 3 " +
+            "AND EMAIL.OBJECT_ID = U.OBJECT_ID " +
+            // 1 = FIRST_NAME
+            "AND FIRST_NAME.ATTRN_ID = 1 " +
+            "AND FIRST_NAME.OBJECT_ID = U.OBJECT_ID " +
+            // 2 = LAST_NAME
+            "AND LAST_NAME.ATTRN_ID = 2 " +
+            "AND LAST_NAME.OBJECT_ID = U.OBJECT_ID " +
+            // 4 = ROLE
+            "AND USER_ROLE_ID.ATTRN_ID = 4 " +
+            "AND USER_ROLE_ID.OBJECT_ID = U.OBJECT_ID " +
+            "AND USER_ROLE.LIST_VALUE_ID = USER_ROLE_ID.LIST_VALUE_ID " +
+            // 5 = CREATION_DATE
+            "AND HIRE_DATE.ATTRN_ID = 5 " +
+            "AND HIRE_DATE.OBJECT_ID = U.OBJECT_ID " +
+            // 7 = STATUS
+            "AND STATUS_ID.ATTRN_ID = 7 " +
+            "AND STATUS_ID.OBJECT_ID = U.OBJECT_ID " +
+            "AND STATUS.LIST_VALUE_ID = STATUS_ID.LIST_VALUE_ID;";
+
+    String READ_USER_BY_LOGIN = "SELECT " +
+            "U.OBJECT_ID AS USER_ID, " +
+            "U.NAME AS LOGIN, " +
+            "PASS.VALUE AS PASSWORD, " +
+            "EMAIL.VALUE AS EMAIL, " +
+            "FIRST_NAME.VALUE AS FIRST_NAME, " +
+            "LAST_NAME.VALUE AS LAST_NAME, " +
+            "USER_ROLE.VALUE AS USER_ROLE, " +
+            "HIRE_DATE.DATE_VALUE AS HIRE_DATE, " +
+            "STATUS.VALUE AS STATUS, " +
+            "CASE WHEN EXISTS (" +
+            "SELECT U.OBJECT_ID" +
+            "FROM OBJECTS U, " +
+            "OBJREFERENCE PROJ_ID, " +
+            "OBJECTS PROJ, " +
+            "ATTRIBUTES PROJ_STATUS " +
+            // 1 = USER
+            "WHERE U.OBJECT_TYPE_ID = 1 " +
+            "AND U.NAME = ? " +
+            // 26 = PROJECT_USERS
+            "AND PROJ_ID.ATTRN_ID = 26 " +
+            "AND PROJ_ID.REFERENCE = U.OBJECT_ID " +
+            // 2 = PROJECT
+            "AND PROJ.OBJECT_TYPE_ID = 2 " +
+            "AND PROJ.OBJECT_ID = PROJ_ID.OBJECT_ID " +
+            // 7 = STATUS
+            "AND PROJ_STATUS.ATTRN_ID = 7 " +
+            "AND PROJ_STATUS.OBJECT_ID = PROJ.OBJECT_ID " +
+            // 5 = ACTIVE
+            "AND PROJ_STATUS.LIST_VALUE_ID = 5" +
+            ") THEN (" +
+            "SELECT PROJ.OBJECT_ID " +
+            "FROM OBJECTS U, " +
+            "OBJREFERENCE PROJ_ID, " +
+            "OBJECTS PROJ, " +
+            "ATTRIBUTES PROJ_STATUS " +
+            // 1 = USER
+            "WHERE U.OBJECT_TYPE_ID = 1 " +
+            "AND U.NAME = ? " +
+            // 26 = PROJECT_USERS
+            "AND PROJ_ID.ATTRN_ID = 26 " +
+            "AND PROJ_ID.REFERENCE = U.OBJECT_ID " +
+            // 2 = PROJECT
+            "AND PROJ.OBJECT_TYPE_ID = 2 " +
+            "AND PROJ.OBJECT_ID = PROJ_ID.OBJECT_ID " +
+            // 7 = STATUS
+            "AND PROJ_STATUS.ATTRN_ID = 7 " +
+            "AND PROJ_STATUS.OBJECT_ID = PROJ.OBJECT_ID " +
+            // 5 = ACTIVE
+            "AND PROJ_STATUS.LIST_VALUE_ID = 5 " +
+            ") ELSE NULL " +
+            "END AS PROJ_ID " +
+            "FROM OBJECTS U, " +
+            "ATTRIBUTES PASS, " +
+            "ATTRIBUTES EMAIL, " +
+            "ATTRIBUTES FIRST_NAME, " +
+            "ATTRIBUTES LAST_NAME, " +
+            "LISTS USER_ROLE, ATTRIBUTES USER_ROLE_ID, " +
+            "LISTS STATUS, ATTRIBUTES STATUS_ID " +
+            // 1 = USER
+            "WHERE U.OBJECT_TYPE_ID = 1 " +
+            "AND U.NAME = ? " +
+            // 6 = PASSWORD
+            "AND PASS.ATTRN_ID = 6 " +
+            "AND PASS.OBJECT_ID = U.OBJECT_ID " +
+            // 3 = EMAIL
+            "AND EMAIL.ATTRN_ID = 3 " +
+            "AND EMAIL.OBJECT_ID = U.OBJECT_ID " +
+            // 1 = FIRST_NAME
+            "AND FIRST_NAME.ATTRN_ID = 1 " +
+            "AND FIRST_NAME.OBJECT_ID = U.OBJECT_ID " +
+            // 2 = LAST_NAME
+            "AND LAST_NAME.ATTRN_ID = 2 " +
+            "AND LAST_NAME.OBJECT_ID = U.OBJECT_ID " +
+            // 4 = ROLE
+            "AND USER_ROLE_ID.ATTRN_ID = 4 " +
+            "AND USER_ROLE_ID.OBJECT_ID = U.OBJECT_ID " +
+            "AND USER_ROLE.LIST_VALUE_ID = USER_ROLE_ID.LIST_VALUE_ID " +
+            // 5 = CREATION_DATE
+            "AND HIRE_DATE.ATTRN_ID = 5 " +
+            "AND HIRE_DATE.OBJECT_ID = U.OBJECT_ID " +
+            // 7 = STATUS
+            "AND STATUS_ID.ATTRN_ID = 7 " +
+            "AND STATUS_ID.OBJECT_ID = U.OBJECT_ID " +
+            "AND STATUS.LIST_VALUE_ID = STATUS_ID.LIST_VALUE_ID;";
+
+    String UPDATE_USER_PASSWORD = "UPDATE " +
+            "ATTRIBUTES SET VALUE = ? " +
+            // 6 = PASSWORD
+            "WHERE ATTRN_ID = 6 " +
+            "AND OBJECT_ID = ?;";
+
+    String UPDATE_USER_FIRST_NAME = "UPDATE " +
+            "ATTRIBUTES SET VALUE = ? " +
+            // 1 = FIRST_NAME
+            "WHERE ATTRN_ID = 1 " +
+            "AND OBJECT_ID = ?;";
+
+    String UPDATE_USER_LAST_NAME = "UPDATE " +
+            "ATTRIBUTES SET VALUE = ? " +
+            // 2 = LAST_NAME
+            "WHERE ATTRN_ID = 2 " +
+            "AND OBJECT_ID = ?;";
+
+    String UPDATE_USER_ROLE = "UPDATE " +
+            "ATTRIBUTES SET LIST_VALUE_ID = ? " +
+            // 4 = ROLE
+            "WHERE ATTRN_ID = 4 " +
+            "AND OBJECT_ID = ?;";
+
+    String INACTIVE_USER = "UPDATE " +
+            // 6 = INACTIVE
+            "ATTRIBUTES SET LIST_VALUE_ID = 6 " +
+            // 7 = STATUS
+            "WHERE ATTRN_ID = 7 " +
+            "AND OBJECT_ID = ?;";
+
 }
--- src/main/java/com/devstr/dao/impl/UserDAOImpl.java
+++ src/main/java/com/devstr/dao/impl/UserDAOImpl.java
@@ -2,35 +2,104 @@ package com.devstr.dao.impl;
 
 import com.devstr.dao.UserDAO;
 import com.devstr.model.User;
+import com.devstr.model.enumerations.AttributeID;
+import com.devstr.model.enumerations.Status;
 import com.devstr.model.enumerations.UserRole;
-
+import com.devstr.model.impl.UserImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+@Transactional
+@Repository
 public class UserDAOImpl implements UserDAO {
 
-    @Override
-    public void createUser(String login, String firstName, String lastName, String email, String password, UserRole userRole) {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
+    @Override
+    public void createUser(String login,
+                           String firstName,
+                           String lastName,
+                           String email,
+                           UserRole userRole,
+                           String password) {
+        jdbcTemplate.update(CREATE_USER, new Object[]{
+                login,
+                firstName,
+                lastName,
+                email,
+                userRole.getId().longValue(),
+                password
+        });
     }
 
     @Override
     public User readUserById(BigInteger id) {
-        return null;
+        RowMapper<User> mapper = new UserRowMapper();
+        return jdbcTemplate.queryForObject(READ_USER_BY_ID, mapper, new Object[]{
+                id.longValue(),
+                id.longValue(),
+                id.longValue()
+        });
     }
 
     @Override
     public User readUserByLogin(String login) {
-        return null;
+        RowMapper<User> mapper = new UserRowMapper();
+        return jdbcTemplate.queryForObject(READ_USER_BY_ID, mapper, new Object[]{
+                login,
+                login,
+                login
+        });
     }
 
     @Override
-    public void updateUser(User user) {
-
+    public void updateUserPassword(BigInteger id, String password) {
+        jdbcTemplate.update(UPDATE_USER_PASSWORD, id.longValue(), password);
     }
 
     @Override
-    public void inactivateUser(User user) {
+    public void updateUserFirstName(BigInteger id, String firstName) {
+        jdbcTemplate.update(UPDATE_USER_FIRST_NAME, id.longValue(), firstName);
+    }
 
+    @Override
+    public void updateUserLastName(BigInteger id, String lastName) {
+        jdbcTemplate.update(UPDATE_USER_FIRST_NAME, id.longValue(), lastName);
+    }
+
+    @Override
+    public void updateUserRole(BigInteger id, UserRole role) {
+        jdbcTemplate.update(UPDATE_USER_ROLE, id.longValue(), role.getId().longValue());
+    }
+
+    @Override
+    public void inactivateUser(BigInteger id) {
+        jdbcTemplate.update(INACTIVE_USER, id.longValue());
+    }
+
+    class UserRowMapper implements RowMapper<User> {
+        @Override
+        public User mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            return new UserImpl
+                    .Builder(resultSet.getString(2),
+                             resultSet.getString(3),
+                             resultSet.getString(4),
+                             resultSet.getString(5),
+                             resultSet.getString(6),
+                             UserRole.valueOf(resultSet.getString(7)),
+                             Status.valueOf(resultSet.getString(9)))
+                    .setUserId(BigInteger.valueOf(resultSet.getLong(1)))
+                    .setHireDate(resultSet.getDate(8))
+                    .setProjectId(BigInteger.valueOf(resultSet.getLong(10)))
+                    .build();
+        }
     }
 
 }
