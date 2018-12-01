105cad01c399aaa1a907aa293f88357cd5d5e6cf
ColdMentha <31902812+ColdMentha@users.noreply.github.com>
2018-12-02 01:41:13 +0200

    https://nc-std.atlassian.net/projects/DEVSTR/issues/DEVSTR-50 Add Token Service Implementation

---
+++ src/main/java/com/devstr/services/impl/EncryptionTokenImp.java
@@ -0,0 +1,80 @@
+package com.devstr.services.impl;
+
+import com.devstr.services.EncryptionToken;
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.ByteBuffer;
+import java.security.AlgorithmParameters;
+import java.security.SecureRandom;
+
+public class EncryptionTokenImp implements EncryptionToken {
+    private String password="mazepinzi16391709bendery";
+
+    @Override
+    public String encrypt(String text) throws Exception {
+        byte[] ivBytes;
+        SecureRandom rendom = new SecureRandom();
+        byte[] bytes = new byte[20];
+        rendom.nextBytes(bytes);
+        byte[] saltBytes = bytes;
+
+        SecretKeyFactory facroty = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), saltBytes, 65536,256);
+
+        SecretKey secretKey = facroty.generateSecret(spec);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+        AlgorithmParameters params = cipher.getParameters();
+        ivBytes = params.getParameterSpec(IvParameterSpec.class).getIV();
+
+        byte[] encryptedTextBytes = cipher.doFinal(text.getBytes("UTF-8"));
+
+        byte[] buffer = new byte[saltBytes.length + ivBytes.length + encryptedTextBytes.length];
+
+        System.arraycopy(saltBytes, 0, buffer, 0, saltBytes.length);
+        System.arraycopy(ivBytes, 0, buffer, saltBytes.length, ivBytes.length);
+
+        System.arraycopy(encryptedTextBytes, 0 ,buffer, saltBytes.length + ivBytes.length, encryptedTextBytes.length);
+
+
+        return new Base64().encodeToString(buffer);
+    }
+
+    @SuppressWarnings("static-access")
+    @Override
+    public String decrypt(String encryptText) throws Exception {
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+        ByteBuffer buffer = ByteBuffer.wrap(new Base64().decode(encryptText));
+        byte[] saltBytes = new byte[20];
+        buffer.get(saltBytes, 0, saltBytes.length);
+        byte[] ivBytes1 = new byte[cipher.getBlockSize()];
+        buffer.get(ivBytes1, 0, ivBytes1.length);
+        byte[] encryptedTextBytes = new byte[buffer.capacity() - saltBytes.length - ivBytes1.length];
+
+        buffer.get(encryptedTextBytes);
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), saltBytes, 65536, 256);
+        SecretKey secretKey = factory.generateSecret(spec);
+        SecretKeySpec secret = new SecretKeySpec(secretKey.getEncoded(), "AES");
+        cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(ivBytes1));
+        byte[] decryptedTextBytes = null;
+        try {
+            decryptedTextBytes = cipher.doFinal(encryptedTextBytes);
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+        return new String(decryptedTextBytes);
+    }
+}
