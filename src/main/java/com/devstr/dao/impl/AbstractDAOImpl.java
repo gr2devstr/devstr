270f5e31d92e258c27212a319ee8915ec648919f
Nikolay <holinkonik@gmail.com>
2018-12-02 22:42:18 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-73 Abstract DAO implementation changed according with interface. Tested.

--- src/main/java/com/devstr/dao/impl/AbstractDAOImpl.java
+++ src/main/java/com/devstr/dao/impl/AbstractDAOImpl.java
@@ -1,13 +1,22 @@
 package com.devstr.dao.impl;
 
 import com.devstr.dao.AbstractDAO;
+import oracle.jdbc.OracleTypes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SqlOutParameter;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Transactional
 @Repository
@@ -18,87 +27,211 @@ abstract class AbstractDAOImpl implements AbstractDAO {
 
     @Override
     public BigInteger createObject(BigInteger typeId, String name) {
-        return jdbcTemplate.queryForObject(CREATE_OBJECT, new Object[]{typeId, name}, BigInteger.class);
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withFunctionName("insert_object");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_object_type_id", typeId);
+        in.put("a_name", name);
+        return call.executeFunction(BigDecimal.class, in).toBigInteger();
     }
 
     @Override
     public String readObjectNameById(BigInteger objectId) {
-        return jdbcTemplate.queryForObject(READ_OBJECT_BY_ID, new Object[]{objectId}, String.class);
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withFunctionName("select_object_name");
+        Map<String, Object> in = new HashMap<>();
+        in.put("id", objectId);
+        return call.executeFunction(String.class, in);
     }
 
     @Override
     public BigInteger readObjectIdByName(BigInteger typeId, String name) {
-        return jdbcTemplate.queryForObject(READ_OBJECT_BY_ID, new Object[]{typeId, name}, BigInteger.class);
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withFunctionName("select_object_id");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_object_type_id", typeId);
+        in.put("a_name", name);
+        return call.executeFunction(BigDecimal.class, in).toBigInteger();
     }
 
     @Override
     public void deleteObjectById(BigInteger objectId) {
-        jdbcTemplate.update(DEL_OBJECT_BY_ID, objectId);
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withProcedureName("delete_object");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_object_id", objectId);
+        call.execute(in);
     }
 
     @Override
     public void createAttributeValue(BigInteger attrnId, BigInteger objectId, String value) {
-        jdbcTemplate.update(CREATE_ATTR_VALUE, new Object[]{attrnId, objectId, value});
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withProcedureName("insert_attribute_value");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_object_id", objectId);
+        in.put("a_value", value);
+        call.execute(in);
     }
 
     @Override
     public void createAttributeDateValue(BigInteger attrnId, BigInteger objectId, Date value) {
-        jdbcTemplate.update(CREATE_ATTR_DATE_VALUE, new Object[]{attrnId, objectId, value});
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withProcedureName("insert_attribute_date_value");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_object_id", objectId);
+        in.put("a_date", value);
+        call.execute(in);
     }
 
     @Override
     public void createAttributeListValue(BigInteger attrnId, BigInteger objectId, BigInteger listValueId) {
-        jdbcTemplate.update(CREATE_ATTR_LIST_VALUE, new Object[]{attrnId, objectId, listValueId});
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withProcedureName("insert_attribute_list_value");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_object_id", objectId);
+        in.put("a_list_value_id", listValueId);
+        call.execute(in);
     }
 
     @Override
     public String readAttributeValue(BigInteger attrnId, BigInteger objectId) {
-        return jdbcTemplate.queryForObject(READ_ATTR_VALUE, new Object[]{attrnId, objectId}, String.class);
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withFunctionName("select_attribute_value");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_object_id", objectId);
+        return call.executeFunction(String.class, in);
     }
 
     @Override
     public Date readAttributeDateValue(BigInteger attrnId, BigInteger objectId) {
-        return jdbcTemplate.queryForObject(READ_ATTR_DATE_VALUE, new Object[]{attrnId, objectId}, Date.class);
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withFunctionName("select_attribute_date_value");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_object_id", objectId);
+        return call.executeFunction(Date.class, in);
     }
 
     @Override
     public String readAttributeListValue(BigInteger attrnId, BigInteger objectId) {
-        return jdbcTemplate.queryForObject(READ_ATTR_LIST_VALUE, new Object[]{attrnId, objectId}, String.class);
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withFunctionName("select_attr_list_value_name");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_object_id", objectId);
+        return call.executeFunction(String.class, in);
     }
 
     @Override
     public void updateAttributeValue(BigInteger attrnId, BigInteger objectId, String value) {
-        jdbcTemplate.update(UPDATE_ATTR_VALUE, new Object[]{attrnId, objectId, value});
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withProcedureName("update_attribute_value");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_object_id", objectId);
+        in.put("new_value", value);
+        call.execute(in);
     }
 
     @Override
     public void updateAttributeDateValue(BigInteger attrnId, BigInteger objectId, Date value) {
-        jdbcTemplate.update(UPDATE_ATTR_VALUE, new Object[]{attrnId, objectId, value});
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withProcedureName("update_attribute_date_value");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_object_id", objectId);
+        in.put("new_value", value);
+        call.execute(in);
     }
 
     @Override
     public void updateAttributeListValue(BigInteger attrnId, BigInteger objectId, BigInteger listValueId) {
-        jdbcTemplate.update(UPDATE_ATTR_VALUE, new Object[]{attrnId, objectId, listValueId});
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withProcedureName("update_attribute_list_value");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_object_id", objectId);
+        in.put("new_list_value_id", listValueId);
+        call.execute(in);
     }
 
     @Override
     public void createObjectReference(BigInteger attrnId, BigInteger objectId, BigInteger referenceId) {
-        jdbcTemplate.update(CREATE_OBJ_REFERENCE, new Object[]{attrnId, objectId, referenceId});
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withProcedureName("insert_objreference");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_object_id", objectId);
+        in.put("a_reference", referenceId);
+        call.execute(in);
     }
 
     @Override
     public Collection<BigInteger> readObjectReferences(BigInteger attrnId, BigInteger objectId) {
-        return jdbcTemplate.queryForList(READ_OBJ_REFERENCE_REF, new Object[]{attrnId, objectId}, BigInteger.class);
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withFunctionName("select_objreference_ref")
+                .declareParameters(new SqlOutParameter("return", OracleTypes.CURSOR,
+                        new RowMapper<BigInteger>() {
+                            @Override
+                            public BigInteger mapRow(ResultSet resultSet, int i) throws SQLException {
+                                return BigInteger.valueOf(resultSet.getLong(1));
+                            }
+                        }));
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_object_id", objectId);
+        Map<String, Object> resultMap = call.execute(in);
+        return (Collection<BigInteger>) resultMap.get("return");
     }
 
     @Override
-    public BigInteger readObjectByReference(BigInteger attrnId, BigInteger referenceId) {
-        return jdbcTemplate.queryForObject(READ_OBJ_REFERENCE_OBJ, new Object[]{attrnId, referenceId}, BigInteger.class);
+    public Collection<BigInteger> readObjectByReference(BigInteger attrnId, BigInteger referenceId) {
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withFunctionName("select_objreference_obj")
+                .declareParameters(new SqlOutParameter("return", OracleTypes.CURSOR,
+                        new RowMapper<BigInteger>() {
+                            @Override
+                            public BigInteger mapRow(ResultSet resultSet, int i) throws SQLException {
+                                return BigInteger.valueOf(resultSet.getLong(1));
+                            }
+                        }));
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_reference", referenceId);
+        Map<String, Object> resultMap = call.execute(in);
+        return (Collection<BigInteger>) resultMap.get("return");
     }
 
     @Override
     public void deleteObjectReference(BigInteger attrnId, BigInteger objectId, BigInteger referenceId) {
-        jdbcTemplate.update(DEL_OBJ_REFERENCE, new Object[]{attrnId, objectId, referenceId});
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("abstract_eav_pkg")
+                .withProcedureName("delete_objreference");
+        Map<String, Object> in = new HashMap<>();
+        in.put("a_attrn_id", attrnId);
+        in.put("a_object_id", objectId);
+        in.put("a_reference", referenceId);
+        call.execute(in);
     }
 
 }
---
+++ src/test/java/com/devstr/dao/impl/AbstractDaoTest.java
@@ -0,0 +1,150 @@
+package com.devstr.dao.impl;
+
+import com.devstr.model.enumerations.AttributeID;
+import com.devstr.model.enumerations.ObjectType;
+import com.devstr.model.enumerations.Status;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import java.math.BigInteger;
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+public class AbstractDaoTest extends AbstractDAOImpl{
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Autowired
+    @Test
+    public void createObjectTest(){
+        String tUser = "user_test";
+        String tProject = "project_test";
+
+        BigInteger userId = createObject(ObjectType.USER.getId(), tUser);
+        BigInteger projId = createObject(ObjectType.PROJECT.getId(), tProject);
+
+        String dbUser = readObjectNameById(userId);
+        String dbProj = readObjectNameById(projId);
+
+        assertEquals(tUser, dbUser);
+        assertEquals(tProject, dbProj);
+        /*Cleans after test*/
+        deleteObjectById(userId);
+        deleteObjectById(projId);
+    }
+
+    @Autowired
+    @Test
+    public void readObjectNameByIdTest(){
+        String loginHn = "holinkonik";
+        String loginTr = "robert_t";
+        String loginRa = "alex_r";
+
+        BigInteger idHn = readObjectIdByName(ObjectType.USER.getId(), loginHn);
+        BigInteger idTr = readObjectIdByName(ObjectType.USER.getId(), loginTr);
+        BigInteger idRa = readObjectIdByName(ObjectType.USER.getId(), loginRa);
+        String dbHn = readObjectNameById(idHn);
+        String dbTr = readObjectNameById(idTr);
+        String dbRa = readObjectNameById(idRa);
+
+        assertEquals(loginHn, dbHn);
+        assertEquals(loginTr, dbTr);
+        assertEquals(loginRa, dbRa);
+    }
+
+    @Autowired
+    @Test
+    public void deleteObjectByIdTest(){
+        String tUser = "user_test";
+
+        BigInteger userId = createObject(ObjectType.USER.getId(), tUser);
+
+        deleteObjectById(userId);
+        String delUser = readObjectNameById(userId);
+
+        assertNull(delUser);
+    }
+
+    @Autowired
+    @Test
+    public void createAndReadAttributesTest(){
+        String tProject = "project_test";
+        String gitLogin = "gr2devstr";
+        String gitPassword = "devstr623824!!";
+        /*
+        TODO: issue with date format
+        Date current = new Date();
+        */
+
+        BigInteger projId = createObject(ObjectType.PROJECT.getId(), tProject);
+        createAttributeValue(AttributeID.GIT_LOGIN.getId(), projId, gitLogin);
+        createAttributeValue(AttributeID.GIT_PASSWORD.getId(), projId, gitPassword);
+        //createAttributeDateValue(AttributeID.CREATION_DATE.getId(), projId, current);
+        createAttributeListValue(AttributeID.STATUS.getId(), projId, Status.ACTIVE.getId());
+
+        String dbProject = readObjectNameById(projId);
+        String dbGitLogin = readAttributeValue(AttributeID.GIT_LOGIN.getId(), projId);
+        String dbGitPassword = readAttributeValue(AttributeID.GIT_PASSWORD.getId(), projId);
+        //Date dbDate = readAttributeDateValue(AttributeID.CREATION_DATE.getId(), projId);
+        String dbStatus = readAttributeListValue(AttributeID.STATUS.getId(), projId);
+
+        assertEquals(tProject, dbProject);
+        assertEquals(gitLogin, dbGitLogin);
+        assertEquals(gitPassword, dbGitPassword);
+        //assertEquals(current, dbDate);
+        assertEquals(Status.ACTIVE, Status.valueOf(dbStatus));
+        /*Cleans after test*/
+        deleteObjectById(projId);
+    }
+
+    @Autowired
+    @Test
+    public void updateAttributesTest(){
+        String tProject = "project_gg";
+        String gitLogin = "gr2devstr";
+        String gitPassword = "devstr623824!!";
+        String gitNewLogin = "gr3airport";
+        String gitNewPass = "sucks##";
+
+        BigInteger projId = createObject(ObjectType.USER.getId(), tProject);
+        createAttributeValue(AttributeID.GIT_LOGIN.getId(), projId, gitLogin);
+        createAttributeValue(AttributeID.GIT_PASSWORD.getId(), projId, gitPassword);
+        updateAttributeValue(AttributeID.GIT_LOGIN.getId(), projId, gitNewLogin);
+        updateAttributeValue(AttributeID.GIT_PASSWORD.getId(), projId, gitNewPass);
+
+        String dbProject = readObjectNameById(projId);
+        String dbGitLogin = readAttributeValue(AttributeID.GIT_LOGIN.getId(), projId);
+        String dbGitPassword = readAttributeValue(AttributeID.GIT_PASSWORD.getId(), projId);
+
+        assertEquals(tProject, dbProject);
+        assertEquals(gitNewLogin, dbGitLogin);
+        assertEquals(gitNewPass, dbGitPassword);
+        /*Cleans after test*/
+        deleteObjectById(projId);
+    }
+
+    @Autowired
+    @Test
+    public void getReferencesByObjectId() {
+        BigInteger userId = readObjectIdByName(ObjectType.USER.getId(), "holinkonik");
+        Collection<BigInteger> reviews = readObjectReferences(AttributeID.REVIEWS.getId(), userId);
+
+        BigInteger reviewId1 = BigInteger.valueOf(82L);
+        BigInteger reviewId2 = BigInteger.valueOf(83L);
+
+        List<BigInteger> dbReviews = new ArrayList<>(reviews);
+        Collections.sort(dbReviews);
+
+        assertEquals(reviewId1, dbReviews.get(0));
+        assertEquals(reviewId2, dbReviews.get(1));
+    }
+
+}
