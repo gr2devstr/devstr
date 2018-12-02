5e52b908a54bd83123e8d5cc097df6ac588e9ead
Vlad <vladismat@gmail.com>
2018-12-02 21:06:42 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-71 Updated abstract_eav_pkg.sql: added logging exceptions Methods on update now work as update or insert

--- src/main/resources/database_scripts/plsql_packages/abstract_eav_pkg.sql
+++ src/main/resources/database_scripts/plsql_packages/abstract_eav_pkg.sql
@@ -1,4 +1,6 @@
 CREATE OR REPLACE PACKAGE abstract_eav_pkg IS
+  err_code NUMBER;
+  err_message VARCHAR2(400);
   FUNCTION insert_object(a_object_type_id NUMBER, a_name VARCHAR2) RETURN NUMBER;
   PROCEDURE delete_object(a_object_id NUMBER);
   PROCEDURE insert_objreference(a_attrn_id NUMBER, a_object_id NUMBER, a_reference NUMBER);
@@ -42,65 +44,125 @@ CREATE OR REPLACE PACKAGE BODY abstract_eav_pkg IS
 	RETURN NULL;
 	EXCEPTION
 	  WHEN OTHERS THEN
-	    logger_pkg.log('ERROR', SQLCODE||' failed to insert object '||a_name);
+	    err_code := SQLCODE;
+		err_message := SQLERRM;
+	    logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to insert object '||a_name);
 		RAISE;
 	END;
+	
   PROCEDURE delete_object(a_object_id NUMBER) IS
   BEGIN
       DELETE FROM OBJECTS WHERE OBJECT_ID = a_object_id;
 	  logger_pkg.log('INFO', SQL%ROWCOUNT||' rows deleted');
 	  EXCEPTION
 	      WHEN OTHERS THEN
-		     logger_pkg.log('ERROR', SQLCODE||' failed to delete from objects by id '||a_object_id);
+		     err_code := SQLCODE;
+		     err_message := SQLERRM;
+		     logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to delete from objects by id '||a_object_id);
+			 RAISE;
   END;
   PROCEDURE insert_objreference(a_attrn_id NUMBER, a_object_id NUMBER, a_reference NUMBER) IS
   BEGIN
     INSERT INTO OBJREFERENCE(ATTRN_ID, OBJECT_ID, REFERENCE) VALUES(a_attrn_id, a_object_id, a_reference);
   EXCEPTION
 	  WHEN OTHERS THEN
-	    logger_pkg.log('ERROR', SQLCODE||' failed to insert objreference by attrn_id '||a_attrn_id||', object_id '||a_object_id||', reference'||a_reference);
+	    err_code := SQLCODE;
+		err_message := SQLERRM;
+	    logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to insert objreference by attrn_id '||a_attrn_id||', object_id '||a_object_id||', reference'||a_reference);
+		RAISE;
   END;
+  
   PROCEDURE delete_objreference(a_attrn_id NUMBER, a_object_id NUMBER, a_reference NUMBER) IS 
   BEGIN
     DELETE FROM OBJREFERENCE WHERE ATTRN_ID = a_attrn_id AND OBJECT_ID = a_object_id AND REFERENCE = a_reference;
 	  logger_pkg.log('INFO', SQL%ROWCOUNT||' rows deleted');
 	EXCEPTION
 	    WHEN OTHERS THEN
-	       logger_pkg.log('ERROR', SQLCODE||' failed to delete object '||a_object_id);
+		   err_code := SQLCODE;
+		   err_message := SQLERRM;
+	       logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to delete object '||a_object_id);
+		   RAISE;
   END;
   PROCEDURE insert_attribute_value(a_attrn_id NUMBER, a_object_id NUMBER, a_value VARCHAR2) IS
   BEGIN
     INSERT INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE) VALUES (a_attrn_id, a_object_id, a_value);
+	EXCEPTION
+	    WHEN OTHERS THEN
+		   err_code := SQLCODE;
+		   err_message := SQLERRM;
+	       logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to insert attribute with attrn_id '||a_attrn_id||', object_id '||a_object_id||', value'||a_value);
+		   RAISE;
   END;
+  
   PROCEDURE insert_attribute_date_value(a_attrn_id NUMBER, a_object_id NUMBER, a_date DATE) IS
   BEGIN
     INSERT INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, DATE_VALUE) VALUES (a_attrn_id, a_object_id, a_date);
     EXCEPTION
-	  WHEN OTHERS THEN
-	    logger_pkg.log('ERROR', SQLCODE||' failed to insert attribute '||a_attrn_id||' by object_id '||a_object_id);
+	    WHEN OTHERS THEN
+		   err_code := SQLCODE;
+		   err_message := SQLERRM;
+	       logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to insert date attribute with attrn_id '||a_attrn_id||', object_id '||a_object_id||', date value'||a_date);
+		   RAISE;
   END;
+  
   PROCEDURE insert_attribute_list_value(a_attrn_id NUMBER, a_object_id NUMBER, a_list_value_id NUMBER) IS 
   BEGIN
     INSERT INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, LIST_VALUE_ID) VALUES (a_attrn_id, a_object_id, a_list_value_id);
   EXCEPTION
 	  WHEN OTHERS THEN
-	    logger_pkg.log('ERROR', SQLCODE||' failed to insert attribute '||a_attrn_id||' by object_id '||a_object_id);
+		 err_code := SQLCODE;
+		 err_message := SQLERRM;
+	     logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to insert list attribute with attrn_id '||a_attrn_id||', object_id '||a_object_id||', list value'||a_list_value_id);
+		 RAISE;
   END;
+  
   PROCEDURE update_attribute_value(a_attrn_id NUMBER, a_object_id NUMBER, new_value VARCHAR2) IS
   BEGIN
-	UPDATE ATTRIBUTES SET VALUE = new_value WHERE ATTRN_ID = a_attrn_id AND OBJECT_ID = a_object_id;
-	logger_pkg.log('INFO', SQL%ROWCOUNT||' attribute rows updated');
+    INSERT INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, VALUE) VALUES (a_attrn_id, a_object_id, new_value);
+	logger_pkg.log('INFO', SQL%ROWCOUNT||' attribute rows inserted with attrn_id '||a_attrn_id||' and object_id '||a_object_id);
+	EXCEPTION
+	  WHEN DUP_VAL_ON_INDEX THEN
+	    UPDATE ATTRIBUTES SET VALUE = new_value WHERE ATTRN_ID = a_attrn_id AND OBJECT_ID = a_object_id;
+	    logger_pkg.log('INFO', SQL%ROWCOUNT||' attribute rows updated with attrn_id '||a_attrn_id||' and object_id '||a_object_id);
+	
+      WHEN OTHERS THEN
+   	    err_code := SQLCODE;
+	    err_message := SQLERRM;
+	    logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to insert or update attribute with attrn_id '||a_attrn_id||', object_id '||a_object_id||', value'||new_value);
+		RAISE;
   END;
+  
   PROCEDURE update_attribute_date_value(a_attrn_id NUMBER, a_object_id NUMBER, new_date DATE) IS
   BEGIN
-    UPDATE ATTRIBUTES SET DATE_VALUE = new_date WHERE ATTRN_ID = a_attrn_id AND OBJECT_ID = a_object_id;
-	logger_pkg.log('INFO', SQL%ROWCOUNT||' attribute rows updated');
+    INSERT INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, DATE_VALUE) VALUES (a_attrn_id, a_object_id, new_date);
+	logger_pkg.log('INFO', SQL%ROWCOUNT||' date attribute rows inserted with attrn_id '||a_attrn_id||' and object_id '||a_object_id);
+	EXCEPTION
+	  WHEN DUP_VAL_ON_INDEX THEN
+        UPDATE ATTRIBUTES SET DATE_VALUE = new_date WHERE ATTRN_ID = a_attrn_id AND OBJECT_ID = a_object_id;
+	    logger_pkg.log('INFO', SQL%ROWCOUNT||' date attribute rows updated with attrn_id '||a_attrn_id||' and object_id '||a_object_id);
+	    
+	  WHEN OTHERS THEN
+	    err_code := SQLCODE;
+  	    err_message := SQLERRM;
+        logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to insert or update date attribute with attrn_id '||a_attrn_id||', object_id '||a_object_id||', date value'||new_date);
+		RAISE;
   END;
+  
   PROCEDURE update_attribute_list_value(a_attrn_id NUMBER, a_object_id NUMBER, new_list_value_id NUMBER) IS
   BEGIN
-    UPDATE ATTRIBUTES SET LIST_VALUE_ID = new_list_value_id WHERE ATTRN_ID = a_attrn_id AND OBJECT_ID = a_object_id;
-	logger_pkg.log('INFO', SQL%ROWCOUNT||' attribute rows updated');
+    INSERT INTO ATTRIBUTES(ATTRN_ID, OBJECT_ID, LIST_VALUE_ID) VALUES (a_attrn_id, a_object_id, new_list_value_id);
+	logger_pkg.log('INFO', SQL%ROWCOUNT||' list attribute rows inserted with attrn_id '||a_attrn_id||' and object_id '||a_object_id);
+	EXCEPTION
+	  WHEN DUP_VAL_ON_INDEX THEN
+        UPDATE ATTRIBUTES SET LIST_VALUE_ID = new_list_value_id WHERE ATTRN_ID = a_attrn_id AND OBJECT_ID = a_object_id;
+	    logger_pkg.log('INFO', SQL%ROWCOUNT||' list attribute rows updated with attrn_id '||a_attrn_id||' and object_id '||a_object_id);
+	  WHEN OTHERS THEN
+		 err_code := SQLCODE;
+		 err_message := SQLERRM;
+	     logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to insert or update list attribute with attrn_id '||a_attrn_id||', object_id '||a_object_id||', date value'||new_list_value_id);
+		 RAISE;
   END;
+  
   FUNCTION select_object_name(id NUMBER) RETURN VARCHAR2 IS
     res VARCHAR2(2000 BYTE);
   BEGIN
@@ -110,7 +172,13 @@ CREATE OR REPLACE PACKAGE BODY abstract_eav_pkg IS
 	  WHEN NO_DATA_FOUND THEN
 	    logger_pkg.log('INFO', 'no objects selected by id '||id);
 	    RETURN NULL;
+	  WHEN OTHERS THEN
+		 err_code := SQLCODE;
+		 err_message := SQLERRM;
+	     logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to select object with object_id '||id);
+		 RAISE;
   END;
+  
   FUNCTION select_object_id(a_object_type_id NUMBER, a_name VARCHAR2) RETURN NUMBER IS
   res NUMBER;
   BEGIN
@@ -120,7 +188,13 @@ CREATE OR REPLACE PACKAGE BODY abstract_eav_pkg IS
 	  WHEN NO_DATA_FOUND THEN
 	    logger_pkg.log('INFO', 'no objects selected by OT '||a_object_type_id||' and name '||a_name);
 	    RETURN NULL;
+	  WHEN OTHERS THEN
+		 err_code := SQLCODE;
+		 err_message := SQLERRM;
+	     logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to select object with object_type_id '||a_object_type_id||' and name '||a_name);
+		 RAISE;
   END;
+  
   FUNCTION select_attribute_value(a_attrn_id NUMBER, a_object_id NUMBER) RETURN VARCHAR2 IS
   res VARCHAR2(4000 BYTE);
   BEGIN
@@ -130,7 +204,13 @@ CREATE OR REPLACE PACKAGE BODY abstract_eav_pkg IS
 	  WHEN NO_DATA_FOUND THEN
 	    logger_pkg.log('INFO', 'no attributes selected by attrn_id '||a_attrn_id||' and object_id '||a_object_id);
 	    RETURN NULL;
+	  WHEN OTHERS THEN
+		err_code := SQLCODE;
+		err_message := SQLERRM;
+	    logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to select attribute with attrn_id '||a_attrn_id||' and a_object_id '||a_object_id);
+		RAISE;
   END;
+  
   FUNCTION select_attribute_date_value(a_attrn_id NUMBER, a_object_id NUMBER) RETURN DATE IS
   res VARCHAR2(4000 BYTE);
   BEGIN
@@ -140,7 +220,13 @@ CREATE OR REPLACE PACKAGE BODY abstract_eav_pkg IS
 	  WHEN NO_DATA_FOUND THEN
 	    logger_pkg.log('INFO', 'no date attributes selected by attrn_id '||a_attrn_id||' and object_id '||a_object_id);
 	    RETURN NULL;
+	  WHEN OTHERS THEN
+		err_code := SQLCODE;
+		err_message := SQLERRM;
+	    logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to select date attribute with attrn_id '||a_attrn_id||' and a_object_id '||a_object_id);
+		RAISE;
   END;
+  
   FUNCTION select_attribute_list_value_id(a_attrn_id NUMBER, a_object_id NUMBER) RETURN NUMBER IS
   res VARCHAR2(4000 BYTE);
   BEGIN
@@ -150,7 +236,13 @@ CREATE OR REPLACE PACKAGE BODY abstract_eav_pkg IS
 	  WHEN NO_DATA_FOUND THEN
 	    logger_pkg.log('INFO', 'no list attributes selected by attrn_id '||a_attrn_id||' and object_id '||a_object_id);
 	    RETURN NULL;
+	  WHEN OTHERS THEN
+		err_code := SQLCODE;
+		err_message := SQLERRM;
+	    logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to select list attribute with attrn_id '||a_attrn_id||' and a_object_id '||a_object_id);
+		RAISE;
   END;
+  
   FUNCTION select_objreference_obj(a_attrn_id NUMBER, a_reference NUMBER) RETURN sys_refcursor IS
     c sys_refcursor;
   BEGIN
@@ -161,7 +253,13 @@ CREATE OR REPLACE PACKAGE BODY abstract_eav_pkg IS
 	  WHEN NO_DATA_FOUND THEN
 	    logger_pkg.log('INFO', 'no references selected by attrn_id '||a_attrn_id||' and reference '||a_reference);
 	    RETURN NULL;
+	  WHEN OTHERS THEN
+		err_code := SQLCODE;
+		err_message := SQLERRM;
+	    logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to select objreference with attrn_id '||a_attrn_id||' and reference '||a_reference);
+		RAISE;
   END;
+  
   FUNCTION select_objreference_ref(a_attrn_id NUMBER, a_object_id NUMBER) RETURN sys_refcursor IS
   c sys_refcursor;
   BEGIN
@@ -172,6 +270,11 @@ CREATE OR REPLACE PACKAGE BODY abstract_eav_pkg IS
 	  WHEN NO_DATA_FOUND THEN
 	    logger_pkg.log('INFO', 'no references selected by attrn_id '||a_attrn_id||' and object_id '||a_object_id);
 	    RETURN NULL;
+	  WHEN OTHERS THEN
+		err_code := SQLCODE;
+		err_message := SQLERRM;
+	    logger_pkg.log('ERROR', err_code||': '||err_message||'; failed to select objreference with attrn_id '||a_attrn_id||' and a_object_id '||a_object_id);
+		RAISE;
   END;
 END abstract_eav_pkg;
 /
