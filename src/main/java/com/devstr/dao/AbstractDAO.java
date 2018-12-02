328fda8bf97c0b645e7967a72b245aa2a18fd475
Nikolay <holinkonik@gmail.com>
2018-12-02 22:35:00 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-72 Changed contracts of methods.

--- src/main/java/com/devstr/dao/AbstractDAO.java
+++ src/main/java/com/devstr/dao/AbstractDAO.java
@@ -8,6 +8,8 @@ public interface AbstractDAO {
 
     /**
      * Creates new row in the OBJECTS table from the DB
+     * Use SpringJdbcCall for PL\SQL Function:
+     * FUNCTION insert_object(a_object_type_id NUMBER, a_name VARCHAR2) RETURN NUMBER;
      *
      * @param typeId object's type ID in the DB (objects.object_type_id)
      * @param name   object's name in the DB (objects.name)
@@ -16,6 +18,8 @@ public interface AbstractDAO {
 
     /**
      * Reads object's name by ID from OBJECTS table from the DB
+     * Use SpringJdbcCall for PL\SQL Function:
+     * FUNCTION select_object_name(id NUMBER) RETURN VARCHAR2;
      *
      * @param objectId object's ID in the DB (objects.object_id)
      */
@@ -23,6 +27,8 @@ public interface AbstractDAO {
 
     /**
      * Reads object's ID by name and object type ID from OBJECTS table from the DB
+     * Use SpringJdbcCall for PL\SQL Function:
+     * FUNCTION select_object_id(a_object_type_id NUMBER, a_name VARCHAR2) RETURN NUMBER;
      *
      * @param typeId object's type ID in the DB (objects.object_type_id)
      * @param name   object's name in the DB (objects.name)
@@ -31,6 +37,8 @@ public interface AbstractDAO {
 
     /**
      * Deletes object by ID from OBJECTS table from the DB
+     * Use SpringJdbcCall for PL\SQL Procedure:
+     * PROCEDURE delete_object(a_object_id NUMBER);
      *
      * @param objectId object's name in the DB (objects.name)
      */
@@ -38,6 +46,8 @@ public interface AbstractDAO {
 
     /**
      * Creates new attribute's value by object ID and attrn ID in the ATTRIBUTES table from the DB
+     * Use SpringJdbcCall for PL\SQL Procedure:
+     * PROCEDURE insert_attribute_value(a_attrn_id NUMBER, a_object_id NUMBER, a_value VARCHAR2);
      *
      * @param attrnId  attributes's name ID in the DB (attributes.attrn_id)
      * @param objectId object's ID in the DB (attributes.object_id)
@@ -47,6 +57,8 @@ public interface AbstractDAO {
 
     /**
      * Creates new attribute's date value by object ID and attrn ID in the ATTRIBUTES table from the DB
+     * Use SpringJdbcCall for PL\SQL Procedure:
+     * PROCEDURE insert_attribute_date_value(a_attrn_id NUMBER, a_object_id NUMBER, a_date DATE);
      *
      * @param attrnId  attributes's name ID in the DB (attributes.attrn_id)
      * @param objectId object's ID in the DB (attributes.object_id)
@@ -56,6 +68,8 @@ public interface AbstractDAO {
 
     /**
      * Creates new attribute's list value id by object ID and attrn ID in the ATTRIBUTES table from the DB
+     * Use SpringJdbcCall for PL\SQL Procedure:
+     * PROCEDURE insert_attribute_list_value(a_attrn_id NUMBER, a_object_id NUMBER, a_list_value_id NUMBER);
      *
      * @param attrnId     attributes's name ID in the DB (attributes.attrn_id)
      * @param objectId    object's ID in the DB (attributes.object_id)
@@ -65,6 +79,8 @@ public interface AbstractDAO {
 
     /**
      * Reads attribute's value by object ID and attrn ID in the ATTRIBUTES table from the DB
+     * Use SpringJdbcCall for PL\SQL Function:
+     * FUNCTION select_attribute_value(a_attrn_id NUMBER, a_object_id NUMBER) RETURN VARCHAR2;
      *
      * @param attrnId  attributes's name ID in the DB (attributes.attrn_id)
      * @param objectId object's ID in the DB (attributes.object_id)
@@ -73,6 +89,8 @@ public interface AbstractDAO {
 
     /**
      * Reads attribute's date value by object ID and attrn ID in the ATTRIBUTES table from the DB
+     * Use SpringJdbcCall for PL\SQL Function:
+     * FUNCTION select_attribute_date_value(a_attrn_id NUMBER, a_object_id NUMBER) RETURN DATE;
      *
      * @param attrnId  attributes's name ID in the DB (attributes.attrn_id)
      * @param objectId object's ID in the DB (attributes.object_id)
@@ -81,6 +99,8 @@ public interface AbstractDAO {
 
     /**
      * Reads attribute's list value by object ID and attrn ID in the ATTRIBUTES table from the DB
+     * Use SpringJdbcCall for PL\SQL Function:
+     * FUNCTION select_attribute_list_value_id(a_attrn_id NUMBER, a_object_id NUMBER) RETURN NUMBER;
      *
      * @param attrnId  attributes's name ID in the DB (attributes.attrn_id)
      * @param objectId object's ID in the DB (attributes.object_id)
@@ -89,6 +109,8 @@ public interface AbstractDAO {
 
     /**
      * Updates attribute's value by object ID and attrn ID in the ATTRIBUTES table from the DB
+     * Use SpringJdbcCall for PL\SQL Procedure:
+     * PROCEDURE update_attribute_value(a_attrn_id NUMBER, a_object_id NUMBER, new_value VARCHAR2);
      *
      * @param attrnId  attributes's name ID in the DB (attributes.attrn_id)
      * @param objectId object's ID in the DB (attributes.object_id)
@@ -98,6 +120,8 @@ public interface AbstractDAO {
 
     /**
      * Updates attribute's date value by object ID and attrn ID in the ATTRIBUTES table from the DB
+     * Use SpringJdbcCall for PL\SQL Procedure:
+     * PROCEDURE update_attribute_date_value(a_attrn_id NUMBER, a_object_id NUMBER, new_date DATE);
      *
      * @param attrnId  attributes's name ID in the DB (attributes.attrn_id)
      * @param objectId object's ID in the DB (attributes.object_id)
@@ -107,6 +131,8 @@ public interface AbstractDAO {
 
     /**
      * Updates attribute's list value ID by object ID and attrn ID in the ATTRIBUTES table from the DB
+     * Use SpringJdbcCall for PL\SQL Procedure:
+     * PROCEDURE update_attribute_list_value(a_attrn_id NUMBER, a_object_id NUMBER, new_list_value_id NUMBER);
      *
      * @param attrnId     attributes's name ID in the DB (attributes.attrn_id)
      * @param objectId    object's ID in the DB (attributes.object_id)
@@ -116,6 +142,8 @@ public interface AbstractDAO {
 
     /**
      * Creates object's reference by attrn ID, reference ID and object ID in the OBJREFERENCE table from the DB
+     * Use SpringJdbcCall for PL\SQL Procedure:
+     * PROCEDURE insert_objreference(a_attrn_id NUMBER, a_object_id NUMBER, a_reference NUMBER);
      *
      * @param attrnId     attributes's name ID in the DB (objreference.attrn_id)
      * @param referenceId object's ID for reference object in the DB (objreference.reference)
@@ -125,6 +153,8 @@ public interface AbstractDAO {
 
     /**
      * Reads object's references by attrn ID and object ID in the OBJREFERENCE table from the DB
+     * Use SpringJdbcCall for PL\SQL Function:
+     * FUNCTION select_objreference_ref(a_attrn_id NUMBER, a_object_id NUMBER) RETURN sys_refcursor;
      *
      * @param attrnId  attributes's name ID in the DB (objreference.attrn_id)
      * @param objectId object's ID in the DB (objreference.object_id)
@@ -132,105 +162,24 @@ public interface AbstractDAO {
     Collection<BigInteger> readObjectReferences(BigInteger attrnId, BigInteger objectId);
 
     /**
-     * Reads object ID by reference ID and attrn ID in the OBJREFERENCE table from the DB
+     * Reads objects ID by reference ID and attrn ID in the OBJREFERENCE table from the DB
+     * Use SpringJdbcCall for PL\SQL Function:
+     * FUNCTION select_objreference_obj(a_attrn_id NUMBER, a_reference NUMBER) RETURN sys_refcursor;
      *
      * @param attrnId     attributes's name ID in the DB (objreference.attrn_id)
      * @param referenceId object's ID in the DB (objreference.object_id)
      */
-    BigInteger readObjectByReference(BigInteger attrnId, BigInteger referenceId);
+    Collection<BigInteger> readObjectByReference(BigInteger attrnId, BigInteger referenceId);
 
     /**
      * Deletes object's reference by attrn ID, reference ID and object ID in the OBJREFERENCE table from the DB
+     * Use SpringJdbcCall for PL\SQL Procedure:
+     * PROCEDURE delete_objreference(a_attrn_id NUMBER, a_object_id NUMBER, a_reference NUMBER);
      *
      * @param attrnId     attributes's name ID in the DB (objreference.attrn_id)
      * @param objectId    object's ID in the DB (objreference.object_id)
      * @param referenceId object's ID for reference object in the DB (objreference.reference)
      */
     void deleteObjectReference(BigInteger attrnId, BigInteger objectId, BigInteger referenceId);
-
-    /**
-     * abstract_eav.insert_object(a_object_type_id NUMBER, a_name VARCHAR2) RETURN NUMBER;
-     */
-    String CREATE_OBJECT = "SELECT abstract_eav.insert_object(?, ?) FROM dual;";
-
-    /**
-     * abstract_eav.select_object_id(id NUMBER) RETURN;
-     */
-    String READ_OBJECT_BY_ID = "SELECT abstract_eav.select_object_name(?) FROM dual;";
-
-    /**
-     * abstract_eav.select_object_name(a_object_type_id NUMBER, a_name VARCHAR2) RETURN;
-     */
-    String READ_OBJECT_BY_NAME = "SELECT abstract_eav.select_object_id(?, ?) FROM dual;";
-
-    /**
-     * delete_object(a_object_id NUMBER);
-     */
-    String DEL_OBJECT_BY_ID = "EXEC abstract_eav.delete_object(?);";
-
-    /**
-     * insert_attribute_value(a_attrn_id NUMBER, a_object_id NUMBER, a_value VARCHAR2);
-     */
-    String CREATE_ATTR_VALUE = "EXEC abstract_eav.insert_attribute_value(?, ?, ?);";
-
-    /**
-     * insert_attribute_date_value(a_attrn_id NUMBER, a_object_id NUMBER, a_date DATE);
-     */
-    String CREATE_ATTR_DATE_VALUE = "EXEC abstract_eav.insert_attribute_date_value(?, ?, ?);";
-
-    /**
-     * insert_attribute_list_value(a_attrn_id NUMBER, a_object_id NUMBER, a_list_value_id NUMBER);
-     */
-    String CREATE_ATTR_LIST_VALUE = "EXEC abstract_eav.insert_attribute_list_value(?, ?, ?);";
-
-    /**
-     * select_attribute(a_attrn_id NUMBER, a_object_id NUMBER) RETURN attribute_row;
-     */
-    String READ_ATTR_VALUE = "SELECT abstract_eav.select_attribute(?, ?) FROM dual;";
-
-    /**
-     * select_attribute(a_attrn_id NUMBER, a_object_id NUMBER) RETURN attribute_row;
-     */
-    String READ_ATTR_DATE_VALUE = "SELECT abstract_eav.select_attribute(?, ?) FROM dual;";
-
-    /**
-     * select_attribute(a_attrn_id NUMBER, a_object_id NUMBER) RETURN attribute_row;
-     */
-    String READ_ATTR_LIST_VALUE = "SELECT abstract_eav.select_attribute(?, ?) FROM dual;";
-
-    /**
-     * update_attribute_value(a_attrn_id NUMBER, a_object_id NUMBER, new_value VARCHAR2);
-     */
-    String UPDATE_ATTR_VALUE = "EXEC abstract_eav.update_attribute_value(?, ?, ?);";
-
-    /**
-     * update_attribute_date_value(a_attrn_id NUMBER, a_object_id NUMBER, new_date DATE);
-     */
-    String UPDATE_ATTR_DATE_VALUE = "EXEC abstract_eav.update_attribute_date_value(?, ?, ?);";
-
-    /**
-     * update_attribute_list_value(a_attrn_id NUMBER, a_object_id NUMBER, new_list_value_id NUMBER);
-     */
-    String UPDATE_ATTR_LIST_VALUE = "EXEC abstract_eav.update_attribute_list_value(?, ?, ?);";
-
-    /**
-     * insert_objreference(a_attrn_id NUMBER, a_object_id NUMBER, a_reference NUMBER);
-     */
-    String CREATE_OBJ_REFERENCE = "EXEC abstract_eav.insert_objreference(?, ?, ?);";
-
-    /**
-     * select_objreference_obj(a_attrn_id NUMBER, a_reference NUMBER) RETURN object_row;
-     */
-    String READ_OBJ_REFERENCE_OBJ = "SELECT abstract_eav.select_objreference_obj(?, ?) FROM dual;";
-
-    /**
-     * select_objreference_ref(a_attrn_id NUMBER, a_object_id NUMBER) RETURN object_row;
-     */
-    String READ_OBJ_REFERENCE_REF = "SELECT abstract_eav.select_objreference_ref(?, ?) FROM dual;";
-
-    /**
-     * delete_objreference(a_attrn_id NUMBER, a_object_id NUMBER, a_reference NUMBER);
-     */
-    String DEL_OBJ_REFERENCE = "EXEC abstract_eav.delete_objreference(?, ?, ?);";
 
 }
