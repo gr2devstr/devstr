58c6735eafa8b1151a6a185cdbbcb08b859ebee1
Robert <rtalabishka@gmail.com>
2018-12-03 14:39:51 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-69 Add new attribute (user_project) for user. Indicates the current user project.

--- src/main/resources/database_scripts/5_OT_ATTRNAMES_popul.sql
+++ src/main/resources/database_scripts/5_OT_ATTRNAMES_popul.sql
@@ -1,117 +1,119 @@
-﻿
--- Нереференсные атрибуты пользователя
+﻿-- Нереференсные атрибуты пользователя
 INSERT INTO ATTRNAMES (NAME)
- VALUES ('first_name');
- 
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('last_name');
- 
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('email');
+VALUES ('first_name');
 
 INSERT INTO ATTRNAMES (NAME)
- VALUES ('role');
- 
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('creation_date');
+VALUES ('last_name');
 
 INSERT INTO ATTRNAMES (NAME)
- VALUES ('password');
+VALUES ('email');
 
 INSERT INTO ATTRNAMES (NAME)
- VALUES ('status');
+VALUES ('role');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('creation_date');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('password');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('status');
 
 -- Нереференсные атрибуты проекта:
 
 INSERT INTO ATTRNAMES (NAME)
- VALUES ('to_date');
- 
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('git_login');
- 
- INSERT INTO ATTRNAMES (NAME)
- VALUES ('git_pass');
- 
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('jira_login');
+VALUES ('to_date');
 
 INSERT INTO ATTRNAMES (NAME)
- VALUES ('jira_pass');
- 
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('repository_name');
-
- -- Нереференсные атрибуты сущности "Отзыв"
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('review_text');
- 
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('job_quality');
- 
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('job_amount');
- 
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('communication');
- 
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('xp_quality');
+VALUES ('git_login');
 
 INSERT INTO ATTRNAMES (NAME)
- VALUES ('team_spirit');
- 
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('organization');
- 
- INSERT INTO ATTRNAMES (NAME)
- VALUES ('time_mng');
- 
- -- Нереференсные атрибуты сущности "Токен"
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('service_name');
- 
-INSERT INTO ATTRNAMES (NAME)
- VALUES ('token_project'); -- reference attribute
+VALUES ('git_pass');
 
 INSERT INTO ATTRNAMES (NAME)
- VALUES ('token_code');
+VALUES ('jira_login');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('jira_pass');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('repository_name');
+
+-- Нереференсные атрибуты сущности "Отзыв"
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('review_text');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('job_quality');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('job_amount');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('communication');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('xp_quality');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('team_spirit');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('organization');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('time_mng');
+
+-- Нереференсные атрибуты сущности "Токен"
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('service_name');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('token_project'); -- reference attribute
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('token_code');
 
 -- Референсные атрибуты пользователя:
 -- связь с отзывами
 INSERT INTO ATTRNAMES (NAME)
- VALUES ('reviews');
- 
+VALUES ('reviews');
+
 -- Референсные атрибуты проекта:
- -- связь с работничками
+-- связь с работничками
 INSERT INTO ATTRNAMES (NAME)
- VALUES ('project_users');
- 
- -- Референсные атрибуты отзыва
+VALUES ('project_users');
+
+-- Референсные атрибуты отзыва
 INSERT INTO ATTRNAMES (NAME)
- VALUES ('review_author');
- 
+VALUES ('review_author');
+
 INSERT INTO ATTRNAMES (NAME)
- VALUES ('project');
+VALUES ('project');
+
+INSERT INTO ATTRNAMES (NAME)
+VALUES ('user_project');
 
 --Заполнение объектных типов 
-INSERT INTO OBJTYPE (PARENT_ID,NAME)
- VALUES (NULL,'USER');
- 
-INSERT INTO OBJTYPE (PARENT_ID,NAME)
- VALUES (NULL,'PROJECT');
+INSERT INTO OBJTYPE (PARENT_ID, NAME)
+VALUES (NULL, 'USER');
+
+INSERT INTO OBJTYPE (PARENT_ID, NAME)
+VALUES (NULL, 'PROJECT');
 
 -- Отзыв и оценка 
-INSERT INTO OBJTYPE (PARENT_ID,NAME)
- VALUES (NULL,'REVIEW');
- 
-INSERT INTO OBJTYPE (PARENT_ID,NAME)
- VALUES (31,'USERREVIEW');
- 
-INSERT INTO OBJTYPE (PARENT_ID,NAME)
- VALUES (31,'PROJECTREVIEW');
- 
- -- Token
-INSERT INTO OBJTYPE (PARENT_ID,NAME)
- VALUES (NULL,'TOKEN');
-  
- COMMIT;
+INSERT INTO OBJTYPE (PARENT_ID, NAME)
+VALUES (NULL, 'REVIEW');
+
+INSERT INTO OBJTYPE (PARENT_ID, NAME)
+VALUES (31, 'USERREVIEW');
+
+INSERT INTO OBJTYPE (PARENT_ID, NAME)
+VALUES (31, 'PROJECTREVIEW');
+
+-- Token
+INSERT INTO OBJTYPE (PARENT_ID, NAME)
+VALUES (NULL, 'TOKEN');
+
+COMMIT;
