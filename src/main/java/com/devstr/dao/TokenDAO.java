e2baf58fd683acbf9e4948cdc1b554c2deb0946a
ColdMentha <31902812+ColdMentha@users.noreply.github.com>
2018-12-03 03:32:29 +0200

    https://nc-std.atlassian.net/projects/DEVSTR/issues/DEVSTR-36 Update sql request and change some methods

--- src/main/java/com/devstr/dao/TokenDAO.java
+++ src/main/java/com/devstr/dao/TokenDAO.java
@@ -2,42 +2,40 @@ package com.devstr.dao;
 
 import com.devstr.model.Token;
 
+import java.math.BigInteger;
+import java.util.List;
+
 public interface TokenDAO {
 
-    void createToken(int projectId, String tokensName,String serviceName,String tokenEncode);
+    /**
+     * Create token for Project
+     * @param projectId
+     * @param serviceName
+     * @param tokenEncode
+     */
+    BigInteger createToken(BigInteger projectId, String serviceName, String tokenEncode) throws Exception;
 
-    Token readTokenByProject(int projectId);
+    /**
+     * Get token by Project id
+     * @param projectId
+     * @return
+     */
+    List<Token> readTokenByProject(BigInteger projectId);
 
-    void updateToken(int projectId, String serviceName,String tokenEncode);
+    /**
+     * Update token
+     * @param tokenId
+     * @param tokenEncode
+     */
+    void updateToken(BigInteger tokenId, String tokenEncode);
 
-    String CREATE_TOKEN= "BEGIN" +
-            "INSERT INTO OBJECTS (OBJECT_ID, NAME, OBJECT_TYPE_ID) " +
-            "            VALUES (OBJ_ID.nextval, ?, 6); " +
-            "INSERT ALL " +
-            "            INTO ATTRIBUTES (ATTRN_ID, OBJECT_ID, VALUE) VALUES (22, OBJ_ID.currval, ?)--input token name " +
-            "            INTO ATTRIBUTES (ATTRN_ID, OBJECT_ID, VALUE) VALUES (24, OBJ_ID.currval, ?)--input encode token " +
-            "            INTO OBJREFERENCE (ATTRN_ID, REFERENCE, OBJECT_ID) VALUES (23, ?, OBJ_ID.currval)--input project id " +
-            "            SELECT * FROM dual; " +
-            "END;";
+    /**
+     * Update service name
+     * @param tokenId
+     * @param serviceName
+     */
+    public void updateServiceName(BigInteger tokenId, String serviceName);
 
-    String READ_TOKEN_BY_PROJECT_ID = "SELECT o.NAME AS PROJECT_ID, a.VALUE AS TOKEN_NAME, b.VALUE AS TOKEN " +
-            "FROM OBJECTS o, ATTRIBUTES a,  ATTRIBUTES b, OBJREFERENCE r " +
-            "WHERE o.OBJECT_ID = ? " +
-            "AND o.OBJECT_ID = r.REFERENCE " +
-            "AND r.OBJECT_ID = a.OBJECT_ID " +
-            "AND a.object_id = b.object_id " +
-            "AND a.attrn_id = 22 " +
-            "AND b.attrn_id = 24";
-
-    String UPDATE_TOKEN = "UPDATE ATTRIBUTES " +
-            "    SET ATTRIBUTES.VALUE = ? " +
-            "    WHERE ATTRIBUTES.ATTRN_ID = 24 " +
-            "    AND ATTRIBUTES.OBJECT_ID IN (SELECT " +
-            "                                a.OBJECT_ID " +
-            "                                FROM ATTRIBUTES a, OBJECTS o " +
-            "                                WHERE a.ATTRN_ID = 22 " +
-            "                                AND a.VALUE = ?  " +
-            "                                AND o.OBJECT_ID = ? );";
-
+    String READ_TOKEN_ID_BY_PROJECT_ID = "SELECT OBJECT_ID FROM OBJREFERENCE WHERE REFERENCE=?";
 
 }
