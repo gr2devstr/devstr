33529143c9312f791e9db1d8e25ceaa4119a3922
Robert <rtalabishka@gmail.com>
2018-12-06 22:49:57 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-75 imp jira service

---
+++ src/main/java/com/devstr/services/JiraService.java
@@ -0,0 +1,32 @@
+package com.devstr.services;
+
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.devstr.model.Issue;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public interface JiraService {
+    static final HashSet<String> SEARCH_REST_FIELDS = new HashSet<String>(
+            Arrays.asList(new String[]{"project", "summary", "issuetype",
+                    "status", "created", "updated", "priority", "timetracking",
+                    "versions", "assignee", "worklog", "duedate", "resolved", "resolutiondate", "self", "components"}));
+
+    void setLogin(String login);
+
+    void setPassword(String password);
+
+    Issue getIssueByKey(String issueKey, JiraRestClient restClient) throws URISyntaxException;
+
+    List<Issue> getIssuesByProjectId(BigInteger projectId) throws URISyntaxException, IOException;
+
+    String URI = "https://nc-std.atlassian.net/";
+    Integer START_VLUE = 0;
+    Integer MAX_VLUE = 100000;
+
+
+}
---
+++ src/main/java/com/devstr/services/impl/JiraServiceImpl.java
@@ -0,0 +1,117 @@
+package com.devstr.services.impl;
+
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.SearchRestClient;
+import com.atlassian.jira.rest.client.api.domain.ChangelogGroup;
+import com.atlassian.jira.rest.client.api.domain.ChangelogItem;
+import com.atlassian.jira.rest.client.api.domain.SearchResult;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import com.atlassian.util.concurrent.Promise;
+import com.devstr.dao.impl.UserDAOImpl;
+import com.devstr.model.Issue;
+import com.devstr.model.enumerations.IssuePriority;
+import com.devstr.model.enumerations.IssueStatus;
+import com.devstr.model.enumerations.IssueType;
+import com.devstr.model.impl.IssueImpl;
+import com.devstr.services.JiraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JiraServiceImpl implements JiraService {
+
+    @Autowired
+    private UserDAOImpl userDAO;
+
+    private String login;
+    private String password;
+
+    private JiraRestClient getConnection() throws URISyntaxException {
+        URI jiraServerUri = new URI(URI);
+        return new AsynchronousJiraRestClientFactory().createWithBasicHttpAuthentication(jiraServerUri, login, password);
+    }
+
+    @Override
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public Issue getIssueByKey(String issueKey, JiraRestClient restClient) throws URISyntaxException {
+        List<String> assineeEmail = new ArrayList<>();
+        BigInteger assigneeId;
+        boolean overDate = false;
+
+        Promise<com.atlassian.jira.rest.client.api.domain.Issue> issuePromise = restClient.getIssueClient().getIssue(issueKey);
+        com.atlassian.jira.rest.client.api.domain.Issue issue = issuePromise.claim();
+
+        if (getIssueStatus(issue.getStatus().getName()) == IssueStatus.CLOSED) {
+            for (ChangelogGroup group : issue.getChangelog()) {
+                for (ChangelogItem changelogItem : group.getItems()) {
+                    if (changelogItem.getField().equals("assignee")) {
+                        assineeEmail.add(changelogItem.getFrom());
+                    }
+                }
+            }
+
+            assigneeId = userDAO.readUserIdByEmail(restClient.getUserClient().getUser(assineeEmail.get(0)).claim().getEmailAddress());
+        } else assigneeId = userDAO.readUserIdByEmail(issue.getAssignee().getEmailAddress());
+
+        return new IssueImpl.Builder()
+                .setIssueKey(issue.getKey())
+                .setIssueType(getIssueType(issue.getIssueType().getName()))
+                .setIssueStatus(getIssueStatus(issue.getStatus().getName()))
+                .setIssuePriority(getIssuePriority(issue.getPriority().getName()))
+                .setStartDate(issue.getCreationDate().toDate())
+                .setDueDate(issue.getDueDate().toDate())
+                .setUserId(assigneeId)
+                .setReporterId(userDAO.readUserIdByEmail(issue.getReporter().getEmailAddress()))
+                .setOverdate(overDate)
+                .build();
+    }
+
+    @Override
+    public List<Issue> getIssuesByProjectId(BigInteger projectId) throws URISyntaxException, IOException {
+        JiraRestClient jiraRestClient = getConnection();
+        List<Issue> issuesList = new ArrayList<>();
+        String jql = String.format("project = %s", userDAO.readObjectNameById(projectId));
+
+        SearchRestClient searchRestClient = getConnection().getSearchClient();
+        SearchResult searchResult = searchRestClient.searchJql(jql, MAX_VLUE, START_VLUE, SEARCH_REST_FIELDS).claim();
+        Iterable<com.atlassian.jira.rest.client.api.domain.Issue> issues = searchResult.getIssues();
+        for (com.atlassian.jira.rest.client.api.domain.Issue issue : issues) {
+            issuesList.add(getIssueByKey(issue.getKey(), jiraRestClient));
+        }
+        jiraRestClient.close();
+        return issuesList;
+    }
+
+    private IssueType getIssueType(String type) {
+        return IssueType.valueOf(modify(type));
+    }
+
+    private IssueStatus getIssueStatus(String status) {
+        return IssueStatus.valueOf(modify(status));
+    }
+
+    private IssuePriority getIssuePriority(String priority) {
+        return IssuePriority.valueOf(modify(priority));
+    }
+
+    private String modify(String s) {
+        return s.toUpperCase().replace('-', '_').replace(' ', '_');
+    }
+
+}
