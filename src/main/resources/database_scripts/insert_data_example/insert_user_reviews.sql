a4178cba78cb236d755859d44ed377c278333d4b
Nikolay <holinkonik@gmail.com>
2018-12-14 16:36:03 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-40 Reworked insert data for UserReview.

--- src/main/resources/database_scripts/insert_data_example/insert_user_reviews.sql
+++ src/main/resources/database_scripts/insert_data_example/insert_user_reviews.sql
@@ -1,154 +1,130 @@
-DECLARE p_id NUMBER;
 BEGIN
 INSERT INTO objects (name, object_type_id)
-             VALUES ('Review_01', 33);                                  -- 32 = REVIEW.
-p_id := comment_id.currval;             
+             VALUES ('Alex_Review_01', 34);                             -- 34 = USERREVIEW.
 INSERT ALL
-    INTO attributes (attrn_id, object_id, date_value)  
+    INTO attributes (attrn_id, object_id, date_value)
              VALUES (5, comment_id.currval, SYSDATE)                    -- 5 = CREATION_DATE.
     INTO attributes (attrn_id, object_id, value)
-             VALUES (15, comment_id.currval, 'test comment')            -- 14 = REVIEW_TEXT.
+             VALUES (15, comment_id.currval, 'test comment')            -- 15 = REVIEW_TEXT.
     INTO objreference (attrn_id, object_id, reference)
-               VALUES (28, comment_id.currval, auth_id)                 -- 27 = REVIEW_AUTHOR.
-    SELECT o.object_id AS auth_id
-      FROM objects o
-     WHERE o.object_type_id = 31 
-       AND o.name = 'alex_r';
-INSERT INTO objects (name, object_type_id, parent_id)
-             VALUES ('Alex_Review', 34, p_id);                          -- 33 = USERREVIEW.
-INSERT ALL
-    INTO attributes (attrn_id, object_id, value)  
-             VALUES (16, comment_id.currval, '8')                       -- 15 = JOB_QUALITY.  
-    INTO attributes (attrn_id, object_id, value)  
-             VALUES (17, comment_id.currval, '9')                       -- 15 = JOB_AMOUNT.  
-    INTO attributes (attrn_id, object_id, value)  
-             VALUES (18, comment_id.currval, '9')                       -- 16 = COMMUNICATION.              
-    INTO objreference (attrn_id, object_id, reference)  
-               VALUES (29, comment_id.currval, proj_id)                 -- 28 = PROJECT.
+               VALUES (28, comment_id.currval, auth_id)                 -- 28 = REVIEW_AUTHOR.
+    INTO attributes (attrn_id, object_id, value)
+             VALUES (16, comment_id.currval, '8')                       -- 16 = JOB_QUALITY.
+    INTO attributes (attrn_id, object_id, value)
+             VALUES (17, comment_id.currval, '9')                       -- 17 = JOB_AMOUNT.
+    INTO attributes (attrn_id, object_id, value)
+             VALUES (18, comment_id.currval, '9')                       -- 18 = COMMUNICATION.
     INTO objreference (attrn_id, object_id, reference)
-               VALUES (26, reciever_id, comment_id.currval)             -- 25 = REVIEW REFERENCE.
+               VALUES (29, comment_id.currval, proj_id)                 -- 29 = PROJECT.
+    INTO objreference (attrn_id, object_id, reference)
+               VALUES (26, reciever_id, comment_id.currval)             -- 26 = REVIEW REFERENCE.
     SELECT p.object_id AS proj_id,
+           o.object_id AS auth_id,
            u.object_id AS reciever_id
-      FROM objects p, objects u
+      FROM objects p, objects o, objects u
      WHERE p.object_type_id = 32
        AND p.name = 'DEVSTR'
+       AND o.object_type_id = 31
+       AND o.name = 'alex_r'
        AND u.object_type_id = 31
        AND u.name = 'oladushek';
 END;
 /
 
-DECLARE p_id NUMBER;
 BEGIN
 INSERT INTO objects (name, object_type_id)
-             VALUES ('Review_02', 33);                                  -- 32 = REVIEW.
-p_id := comment_id.currval;             
+             VALUES ('Robert_Review_02', 34);                           -- 34 = USERREVIEW.
 INSERT ALL
-    INTO attributes (attrn_id, object_id, date_value)  
+    INTO attributes (attrn_id, object_id, date_value)
              VALUES (5, comment_id.currval, SYSDATE)                    -- 5 = CREATION_DATE.
     INTO attributes (attrn_id, object_id, value)
-             VALUES (15, comment_id.currval, 'another test comment')    -- 14 = REVIEW_TEXT.
+             VALUES (15, comment_id.currval, 'another test comment')    -- 15 = REVIEW_TEXT.
     INTO objreference (attrn_id, object_id, reference)
-               VALUES (28, comment_id.currval, auth_id)                 -- 27 = REVIEW_AUTHOR.
-    SELECT o.object_id AS auth_id
-      FROM objects o
-     WHERE o.object_type_id = 31 
-       AND o.name = 'robert_t';
-INSERT INTO objects (name, object_type_id, parent_id)
-             VALUES ('Robert_Review', 34, p_id);                        -- 33 = USERREVIEW.
-INSERT ALL
-    INTO attributes (attrn_id, object_id, value)  
-             VALUES (16, comment_id.currval, '9')                       -- 15 = JOB_QUALITY.  
-    INTO attributes (attrn_id, object_id, value)  
-             VALUES (17, comment_id.currval, '7')                       -- 15 = JOB_AMOUNT.  
-    INTO attributes (attrn_id, object_id, value)  
-             VALUES (18, comment_id.currval, '10')                      -- 16 = COMMUNICATION.              
-    INTO objreference (attrn_id, object_id, reference)  
-               VALUES (29, comment_id.currval, proj_id)                 -- 28 = PROJECT.
+               VALUES (28, comment_id.currval, auth_id)                 -- 28 = REVIEW_AUTHOR.
+    INTO attributes (attrn_id, object_id, value)
+             VALUES (16, comment_id.currval, '9')                       -- 16 = JOB_QUALITY.
+    INTO attributes (attrn_id, object_id, value)
+             VALUES (17, comment_id.currval, '7')                       -- 17 = JOB_AMOUNT.
+    INTO attributes (attrn_id, object_id, value)
+             VALUES (18, comment_id.currval, '10')                      -- 18 = COMMUNICATION.
     INTO objreference (attrn_id, object_id, reference)
-               VALUES (26, reciever_id, comment_id.currval)             -- 25 = REVIEW REFERENCE.
+               VALUES (29, comment_id.currval, proj_id)                 -- 29 = PROJECT.
+    INTO objreference (attrn_id, object_id, reference)
+               VALUES (26, reciever_id, comment_id.currval)             -- 26 = REVIEW REFERENCE.
     SELECT p.object_id AS proj_id,
+           o.object_id AS auth_id,
            u.object_id AS reciever_id
-      FROM objects p, objects u
+      FROM objects p, objects o, objects u
      WHERE p.object_type_id = 32
        AND p.name = 'DEVSTR'
+       AND o.object_type_id = 31
+       AND o.name = 'robert_t'
        AND u.object_type_id = 31
        AND u.name = 'holinkonik';
 END;
-/             
+/
 
-DECLARE p_id NUMBER;
 BEGIN
 INSERT INTO objects (name, object_type_id)
-             VALUES ('Review_03', 33);                                  -- 32 = REVIEW.
-p_id := comment_id.currval;             
+             VALUES ('Nik_Review_03', 34);                                  -- 34 = USERREVIEW.
 INSERT ALL
-    INTO attributes (attrn_id, object_id, date_value)  
+    INTO attributes (attrn_id, object_id, date_value)
              VALUES (5, comment_id.currval, SYSDATE)                    -- 5 = CREATION_DATE.
     INTO attributes (attrn_id, object_id, value)
-             VALUES (15, comment_id.currval, 'some test comment')       -- 14 = REVIEW_TEXT.
+             VALUES (15, comment_id.currval, 'some test comment')       -- 15 = REVIEW_TEXT.
     INTO objreference (attrn_id, object_id, reference)
-               VALUES (28, comment_id.currval, auth_id)                 -- 27 = REVIEW_AUTHOR.
-    SELECT o.object_id AS auth_id
-      FROM objects o
-     WHERE o.object_type_id = 31 
-       AND o.name = 'holinkonik';
-INSERT INTO objects (name, object_type_id, parent_id)
-             VALUES ('Nik_Review', 34, p_id);                           -- 33 = USERREVIEW.
-INSERT ALL
-    INTO attributes (attrn_id, object_id, value)  
-             VALUES (16, comment_id.currval, '9')                       -- 15 = JOB_QUALITY.  
-    INTO attributes (attrn_id, object_id, value)  
-             VALUES (17, comment_id.currval, '10')                      -- 15 = JOB_AMOUNT.  
-    INTO attributes (attrn_id, object_id, value)  
-             VALUES (18, comment_id.currval, '9')                       -- 16 = COMMUNICATION.              
-    INTO objreference (attrn_id, object_id, reference)  
-               VALUES (29, comment_id.currval, proj_id)                 -- 28 = PROJECT.
+               VALUES (28, comment_id.currval, auth_id)                 -- 28 = REVIEW_AUTHOR.
+    INTO attributes (attrn_id, object_id, value)
+             VALUES (16, comment_id.currval, '9')                       -- 16 = JOB_QUALITY.
+    INTO attributes (attrn_id, object_id, value)
+             VALUES (17, comment_id.currval, '10')                      -- 17 = JOB_AMOUNT.
+    INTO attributes (attrn_id, object_id, value)
+             VALUES (18, comment_id.currval, '9')                       -- 18 = COMMUNICATION.
     INTO objreference (attrn_id, object_id, reference)
-               VALUES (26, reciever_id, comment_id.currval)             -- 25 =  REVIEW REFERENCE.
+               VALUES (29, comment_id.currval, proj_id)                 -- 29 = PROJECT.
+    INTO objreference (attrn_id, object_id, reference)
+               VALUES (26, reciever_id, comment_id.currval)             -- 26 =  REVIEW REFERENCE.
     SELECT p.object_id AS proj_id,
+           o.object_id AS auth_id,
            u.object_id AS reciever_id
-      FROM objects p, objects u
+      FROM objects p, objects o, objects u
      WHERE p.object_type_id = 32
        AND p.name = 'DEVSTR'
+       AND o.object_type_id = 31
+       AND o.name = 'holinkonik'
        AND u.object_type_id = 31
        AND u.name = 'oladushek';
 END;
 /
 
-DECLARE p_id NUMBER;
 BEGIN
 INSERT INTO objects (name, object_type_id)
-             VALUES ('Review_04', 33);                                  -- 32 = REVIEW.
-p_id := comment_id.currval;             
+             VALUES ('Ksu_Review_04', 34);                              -- 34 = USERREVIEW.
 INSERT ALL
-    INTO attributes (attrn_id, object_id, date_value)  
+    INTO attributes (attrn_id, object_id, date_value)
              VALUES (5, comment_id.currval, SYSDATE)                    -- 5 = CREATION_DATE.
     INTO attributes (attrn_id, object_id, value)
-             VALUES (15, comment_id.currval, 'any comment')             -- 14 = REVIEW_TEXT.
+             VALUES (15, comment_id.currval, 'any comment')             -- 15 = REVIEW_TEXT.
     INTO objreference (attrn_id, object_id, reference)
-               VALUES (28, comment_id.currval, auth_id)                 -- 27 = REVIEW_AUTHOR.
-    SELECT o.object_id AS auth_id
-      FROM objects o
-     WHERE o.object_type_id = 31 
-       AND o.name = 'ksusha_m';
-INSERT INTO objects (name, object_type_id, parent_id)
-             VALUES ('Ksu_Review', 34, p_id);                           -- 33 = USERREVIEW.
-INSERT ALL
-    INTO attributes (attrn_id, object_id, value)  
-             VALUES (16, comment_id.currval, '6')                       -- 15 = JOB_QUALITY.  
-    INTO attributes (attrn_id, object_id, value)  
-             VALUES (17, comment_id.currval, '6')                       -- 15 = JOB_AMOUNT.  
-    INTO attributes (attrn_id, object_id, value)  
-             VALUES (18, comment_id.currval, '6')                       -- 16 = COMMUNICATION.              
-    INTO objreference (attrn_id, object_id, reference)  
-               VALUES (29, comment_id.currval, proj_id)                 -- 28 = PROJECT.
+               VALUES (28, comment_id.currval, auth_id)                 -- 28 = REVIEW_AUTHOR.
+    INTO attributes (attrn_id, object_id, value)
+             VALUES (16, comment_id.currval, '6')                       -- 16 = JOB_QUALITY.
+    INTO attributes (attrn_id, object_id, value)
+             VALUES (17, comment_id.currval, '6')                       -- 17 = JOB_AMOUNT.
+    INTO attributes (attrn_id, object_id, value)
+             VALUES (18, comment_id.currval, '6')                       -- 18 = COMMUNICATION.
     INTO objreference (attrn_id, object_id, reference)
-               VALUES (26, reciever_id, comment_id.currval)             -- 25 =  REVIEW REFERENCE.
+               VALUES (29, comment_id.currval, proj_id)                 -- 29 = PROJECT.
+    INTO objreference (attrn_id, object_id, reference)
+               VALUES (26, reciever_id, comment_id.currval)             -- 26 =  REVIEW REFERENCE.
     SELECT p.object_id AS proj_id,
+           o.object_id AS auth_id,
            u.object_id AS reciever_id
-      FROM objects p, objects u
+      FROM objects p, objects o, objects u
      WHERE p.object_type_id = 32
        AND p.name = 'DEVSTR'
+       AND o.object_type_id = 31
+       AND o.name = 'ksusha_m'
        AND u.object_type_id = 31
        AND u.name = 'robert_t';
 END;
