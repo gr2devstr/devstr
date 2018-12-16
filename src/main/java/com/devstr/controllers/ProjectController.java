622eefe3d9be2196f56b34ce480aa78e8a863e08
Robert <rtalabishka@gmail.com>
2018-12-17 00:47:39 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-83 add security

--- src/main/java/com/devstr/controllers/ProjectController.java
+++ src/main/java/com/devstr/controllers/ProjectController.java
@@ -2,11 +2,11 @@ package com.devstr.controllers;
 
 import com.devstr.dao.ProjectDAO;
 import com.devstr.model.Project;
-import com.devstr.services.GitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,13 +24,11 @@ public class ProjectController {
     @Autowired
     private ProjectDAO projectDAO;
 
-    @Autowired
-    private GitService gitService;
-
     static {
         Locale.setDefault(Locale.ENGLISH);
     }
 
+    @PreAuthorize("hasAuthority('PROJECT_MANAGER')")
     @RequestMapping(value = "/crateProject/{name}/{pmid}", method = RequestMethod.POST)
     public ResponseEntity<Void> createProject(@PathVariable("name") String name, @PathVariable("pmid") BigInteger managerId, UriComponentsBuilder builder) throws IOException {
         BigInteger flag = projectDAO.createProject(name, managerId);
@@ -42,6 +40,7 @@ public class ProjectController {
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAuthority('PROJECT_MANAGER')")
     @RequestMapping(value = "/crateProject/{name}/{projid}", method = RequestMethod.PUT)
     public ResponseEntity<Void> updateProjectRepositoryName(@PathVariable("name") String name, @PathVariable("projid") BigInteger projectID, UriComponentsBuilder builder) throws IOException {
         projectDAO.updateProjectRepositoryName(projectID, name);
@@ -50,6 +49,7 @@ public class ProjectController {
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAuthority('PROJECT_MANAGER')")
     @RequestMapping(value = "/update/{jiraLogin}/{projid}", method = RequestMethod.PUT)
     public ResponseEntity<Void> updateProjectJiraLogin(@PathVariable("jiraLogin") String name, @PathVariable("projid") BigInteger projectID, UriComponentsBuilder builder) throws IOException {
         projectDAO.updateProjectJiraLogin(projectID, name);
@@ -58,7 +58,7 @@ public class ProjectController {
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
-
+    @PreAuthorize("hasAuthority('PROJECT_MANAGER')")
     @RequestMapping(value = "/update/{jiraPassword}/{projid}", method = RequestMethod.PUT)
     public ResponseEntity<Void> updateProjectJiraPassword(@PathVariable("jiraPassword") String name, @PathVariable("projid") BigInteger projectID, UriComponentsBuilder builder) throws IOException {
         projectDAO.updateProjectJiraPassword(projectID, name);
@@ -67,6 +67,7 @@ public class ProjectController {
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAuthority('PROJECT_MANAGER')")
     @RequestMapping(value = "/update/{domain}/{projid}", method = RequestMethod.PUT)
     public ResponseEntity<Void> updateProjectJiraDomain(@PathVariable("domain") String name, @PathVariable("projid") BigInteger projectID, UriComponentsBuilder builder) throws IOException {
         projectDAO.updateProjectJiraDomain(projectID, name);
@@ -75,9 +76,12 @@ public class ProjectController {
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
-
+    @PreAuthorize("hasAuthority('ADMIN') " +
+            "OR hasAuthority('TECHNICAL_MANAGER') " +
+            "OR hasAuthority('PROJECT_MANAGER') " +
+            "OR hasAuthority('GROUP_MANAGER')")
     @RequestMapping(value = "/read/{projid}", method = RequestMethod.GET)
-    public ResponseEntity<Project> readProjectById(@PathVariable("projid") BigInteger projID) throws IOException {
+    public ResponseEntity<Project> readProjectById(@PathVariable("projid") BigInteger projID) {
         Project project = projectDAO.readProjectById(projID);
         return new ResponseEntity<>(project, HttpStatus.OK);
     }
