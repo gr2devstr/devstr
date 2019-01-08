ac1e9488793e75fb1589d41dff51606725d7e3bd
Nikolay <holinkonik@gmail.com>
2019-01-08 06:54:58 +0200

    https://nc-std.atlassian.net/browse/DEVSTR-89 Ignored AbstractDaoTest.

--- src/test/java/com/devstr/dao/impl/AbstractDaoTest.java
+++ src/test/java/com/devstr/dao/impl/AbstractDaoTest.java
@@ -21,6 +21,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+@Ignore
 @SpringBootTest
 @Transactional(rollbackFor = Exception.class)
 @RunWith(SpringJUnit4ClassRunner.class)
